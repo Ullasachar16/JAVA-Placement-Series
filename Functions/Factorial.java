55eca123e7bbe68b21222397a274e79b6d92aadf
Ullasachar16 <ullasachar16@gmail.com>
2022-12-03 14:58:05 +0530

    Functions

---
+++ Functions/Factorial.java
@@ -0,0 +1,25 @@
+package Functions;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void printfactorial(int n){
+        if(n<0){
+            System.out.println("Invalid number");
+            return;
+        }
+        int fact=1;
+        for(int i=n;i>=1;i--){
+            fact=fact*i;
+        }
+        System.out.println(fact);
+        return;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter n:");
+        int n= sc.nextInt();
+        printfactorial(n);
+    }
+}
