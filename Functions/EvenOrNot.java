6c230e4e1e8fa03d21778d2279526f11aec7b95b
Ullasachar16 <ullasachar16@gmail.com>
2022-12-03 14:57:46 +0530

    Functions

---
+++ Functions/EvenOrNot.java
@@ -0,0 +1,20 @@
+package Functions;
+
+import java.util.Scanner;
+
+public class EvenOrNot {
+    public static void isEven(int n){
+        if(n%2==0){
+            System.out.println("Even");
+        }else{
+            System.out.println("Odd");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter number:");
+        int n= sc.nextInt();
+        isEven(n);
+    }
+}
