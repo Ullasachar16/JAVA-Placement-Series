80ae5be7cb620e9f2b4a66210593a4040fcc2d6a
Ullasachar16 <ullasachar16@gmail.com>
2022-12-03 18:25:06 +0530

    Functions

---
+++ Functions/AverageOfThreeNumbers.java
@@ -0,0 +1,20 @@
+package Functions;
+import java.util.Scanner;
+public class AverageOfThreeNumbers {
+    public static int printAverage(int a,int b,int c){
+        int avg=(a+b+c)/2;
+        return avg;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter value 1:");
+        int a= sc.nextInt();
+        System.out.println("Enter value 2:");
+        int b= sc.nextInt();
+        System.out.println("Enter value 3:");
+        int c= sc.nextInt();
+        System.out.println("Average of 3 numbers is:"+printAverage(a,b,c));
+
+    }
+}
