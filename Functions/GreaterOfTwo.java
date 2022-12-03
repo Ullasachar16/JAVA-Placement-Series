c12d7f814cde7c5ccbdca1d52f4fce792e04a4c6
Ullasachar16 <ullasachar16@gmail.com>
2022-12-03 18:47:24 +0530

    Functions

---
+++ Functions/GreaterOfTwo.java
@@ -0,0 +1,22 @@
+package Functions;
+
+import java.util.Scanner;
+
+public class GreaterOfTwo {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter value 1:");
+        int a= sc.nextInt();
+        System.out.println("Enter value 2:");
+        int b=sc.nextInt();
+        printGreaterOfTwo(a,b);
+
+    }
+    public static void printGreaterOfTwo(int a,int b){
+        if(a>b){
+            System.out.println("a is greater than b");
+        }else{
+            System.out.println("b is greater than a");
+        }
+    }
+}
