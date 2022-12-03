7a48af1eb9169ef3b5f2df594d46bbf7a30e654d
Ullasachar16 <ullasachar16@gmail.com>
2022-12-03 15:00:37 +0530

    Functions

---
+++ BasicFundamentals/PrintingTablesBasic.java
@@ -0,0 +1,15 @@
+package BasicFundamentals;
+
+import java.util.Scanner;
+
+public class PrintingTablesBasic {
+    public static void main(String[] args) {
+        //print tables according to user's input
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter which table to print:");
+        int table=sc.nextInt();
+        for(int i=1;i<=10;i++){
+            System.out.println(table+" X "+i+" = "+(table*i));
+        }
+    }
+}
--- Functions/PrintingTables.java
+++ Functions/PrintingTables.java
@@ -1,15 +1,17 @@
-package BasicFundamentals;
+package Functions;
 
 import java.util.Scanner;
 
 public class PrintingTables {
-    public static void main(String[] args) {
-        //print tables according to user's input
-        Scanner sc=new Scanner(System.in);
-        System.out.println("enter which table to print:");
-        int table=sc.nextInt();
+    public static void printTables(int table){
         for(int i=1;i<=10;i++){
             System.out.println(table+" X "+i+" = "+(table*i));
         }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter which table to print:");
+        int table=sc.nextInt();
+        printTables(table);
     }
 }
