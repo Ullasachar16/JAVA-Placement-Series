2a989e21fedee0af961911a3561d51f8b6e6da0f
Ullasachar16 <ullasachar16@gmail.com>
2022-12-03 18:54:06 +0530

    Functions Continued

---
+++ Functions/CircumferenceOfCircle.java
@@ -0,0 +1,18 @@
+package Functions;
+
+import java.util.Scanner;
+
+public class CircumferenceOfCircle {
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter radius of circle:");
+        double radius=sc.nextDouble();
+        printCircumferenceOfCircle(radius);
+    }
+    public static void printCircumferenceOfCircle(double radius){
+        double pi=3.141592653589793238462643;
+        double circumferenceOfCircle=2*pi*radius;
+        System.out.println("circumferenceOfCircle is:"+circumferenceOfCircle);
+    }
+}
