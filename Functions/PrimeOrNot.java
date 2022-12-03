09ba9af6299d1025f5d3e50c33a0187d0064b2e2
Ullasachar16 <ullasachar16@gmail.com>
2022-12-03 14:58:13 +0530

    Functions

---
+++ Functions/PrimeOrNot.java
@@ -0,0 +1,28 @@
+package Functions;
+
+import java.util.Scanner;
+
+public class PrimeOrNot {
+    public static boolean isPrime(int n){
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i < n/2; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter num:");
+        int n= sc.nextInt();
+        if(isPrime(n)){
+            System.out.println("It is Prime NUmber");
+        }else{
+            System.out.println("Not a Prime NUmber");
+        }
+    }
+}
