e3b821e86e3771c50c91b81548c4661662e7e671
Ullasachar16 <ullasachar16@gmail.com>
2022-12-03 19:10:01 +0530

    Functions Continued

---
+++ Functions/VoteEligibility.java
@@ -0,0 +1,23 @@
+package Functions;
+
+import java.util.Scanner;
+
+public class VoteEligibility {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter age:");
+        int age= sc.nextInt();
+        if(isEligibleToVote(age)){
+            System.out.println("Eligible to vote");
+        }else{
+            System.out.println("Have patience kid!!");
+        }
+    }
+    public static boolean isEligibleToVote(int age){
+        if(age>=18){
+            return true;
+        }else {
+            return false;
+        }
+    }
+}
