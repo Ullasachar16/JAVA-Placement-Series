ea0fbb4e64a54fdc9440493cee8035e257f0045c
Ullasachar16 <ullasachar16@gmail.com>
2022-12-03 14:57:42 +0530

    Functions

---
+++ Functions/CalculateSum.java
@@ -0,0 +1,26 @@
+package Functions;
+
+import java.util.Scanner;
+
+public class CalculateSum {
+    public static int calculateSum(int a,int b){
+        int sum=a+b;
+        return sum;
+    }
+    public static int calculateMul(int a,int b){
+        int mul=a*b;
+        return mul;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a:");
+        int a=sc.nextInt();
+        System.out.print("Enter b:");
+        int b= sc.nextInt();
+        int sum=calculateSum(a,b);
+        System.out.println("sum:"+sum);
+        int mul=calculateMul(a,b);
+        System.out.println("mul:"+mul);
+
+    }
+}
