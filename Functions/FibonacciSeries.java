0c95342efe0bac165617d7c8acfca9cf5e3d364b
Ullasachar16 <ullasachar16@gmail.com>
2022-12-04 13:36:08 +0530

    Functions Continued

---
+++ Functions/FibonacciSeries.java
@@ -0,0 +1,25 @@
+package Functions;
+
+import java.util.Scanner;
+
+public class FibonacciSeries {
+    public static void printFibonacci(int n){
+        int a=0,b=1;
+        System.out.print(a+" ");
+        if(n>1){
+            for(int i=2;i<=n;i++){
+                System.out.print(b+" ");
+                int temp=b;
+                b=a+b;
+                a=temp;
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a value:");
+        int n=sc.nextInt();
+        printFibonacci(n);
+    }
+}
