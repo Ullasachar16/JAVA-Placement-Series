d95a5edc65ab0b9a5ba6e5bc8c331e6d07f9bcfd
Ullasachar16 <ullasachar16@gmail.com>
2022-12-05 13:29:03 +0530

    Function Changes

--- Functions/SumOfOddNumbersTillN.java
+++ Functions/SumOfOddNumbersTillN.java
@@ -5,16 +5,20 @@ import java.util.Scanner;
 public class SumOfOddNumbersTillN {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print(" Please Enter any Number : ");
+        System.out.println(" Please Enter any Number : ");
         int number = sc.nextInt();
         printSumOfOddNum(number);
     }
 
     public static void printSumOfOddNum(int num) {
+        int sum=0;
         for(int i = 1;i <= num;i++) {
             if(i%2!= 0) {
-                System.out.print(i+"\t");
+                System.out.print(i + "\t");
+                sum=sum+i;
             }
         }
+        System.out.println();
+        System.out.println("sum is:"+sum);
     }
 }
