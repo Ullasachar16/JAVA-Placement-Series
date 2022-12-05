db92efdda3e122e70f50ba40f453cd2373aedaa9
Ullasachar16 <ullasachar16@gmail.com>
2022-12-05 12:00:39 +0530

    Function Extras

---
+++ Functions/Product.java
@@ -0,0 +1,13 @@
+package Functions;
+
+public class Product {
+    public static int pro(int a,int b){
+        int c=a*b;
+        return c;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(pro(5,6));
+        System.out.println(pro(15,3));
+    }
+}
