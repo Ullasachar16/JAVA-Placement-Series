97e46e3edc0389a8a0140f22e4b5c075a0fac90d
Ullasachar16 <ullasachar16@gmail.com>
2022-12-05 19:20:55 +0530

    2D Arrays

---
+++ TwoDimensionalArrays/InputtingAndOutputtingArrayElements.java
@@ -0,0 +1,34 @@
+package TwoDimensionalArrays;
+
+import java.util.Scanner;
+
+public class InputtingAndOutputtingArrayElements {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter row size:");
+        int rows=sc.nextInt();
+        System.out.println("Enter col size:");
+        int cols=sc.nextInt();
+
+        int[][] numbers=new int[rows][cols];
+
+        //input
+        //rows
+        System.out.println("Enter elements:");
+        for(int i=0;i<rows;i++){
+            //columns
+
+            for(int j=0;j<cols;j++){
+                numbers[i][j]=sc.nextInt();
+            }
+        }
+        //output
+        System.out.println("Elements are:");
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                System.out.print(numbers[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ TwoDimensionalArrays/SearchGivenNumberAndPrintTheIndex.java
@@ -0,0 +1,31 @@
+package TwoDimensionalArrays;
+
+import java.util.Scanner;
+
+public class SearchGivenNumberAndPrintTheIndex {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter row elements:");
+        int rows= sc.nextInt();
+        System.out.println("Enter col elements:");
+        int cols=sc.nextInt();
+        int numbers[][]=new int[rows][cols];
+
+        //input
+        System.out.println("Enter array elements:");
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                numbers[i][j]=sc.nextInt();
+            }
+        }
+        System.out.println("Enter search element:");
+        int x=sc.nextInt();
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(numbers[i][j]==x){
+                    System.out.println("x found at locations:("+i+ ","+j+")");
+                }
+            }
+        }
+    }
+}
