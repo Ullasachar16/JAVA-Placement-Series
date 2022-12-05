d8c98bf11a8d7721d27d5cc2c183fbe49bff2bf8
Ullasachar16 <ullasachar16@gmail.com>
2022-12-05 18:23:31 +0530

    Array Changes

--- Arrays/MaxAndMinNumberInArray.java
+++ Arrays/MaxAndMinNumberInArray.java
@@ -2,7 +2,7 @@ package Arrays;
 
 import java.util.Scanner;
 
-public class MacAndMinNumberInArray {
+public class MaxAndMinNumberInArray {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter array size:");
