71a0aeae8f31184c840861f6351c366ed3f49fd0
Ullasachar16 <ullasachar16@gmail.com>
2022-12-05 13:19:26 +0530

    Arrays

---
+++ Arrays/CheckArraySortedOrNot.java
@@ -0,0 +1,27 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class CheckArraySortedOrNot {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter array size:");
+        int size=sc.nextInt();
+        int ele[]=new int[size];
+        System.out.println("Enter array elements:");
+        for(int i=0;i<size;i++){
+            ele[i]= sc.nextInt();
+        }
+        boolean isAscending=true;
+        for(int i=0;i< ele.length-1;i++){
+            if(ele[i]>ele[i+1]){
+                isAscending=false;
+            }
+        }
+        if(isAscending){
+            System.out.println("Array is sorted in ascending order");
+        }else{
+            System.out.println("Array is not sorted in ascending order");
+        }
+    }
+}
