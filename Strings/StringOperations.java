aaba8edce22fcb88fe7cdc4384505d1f09bc33f9
Ullasachar16 <ullasachar16@gmail.com>
2022-12-05 20:05:47 +0530

    2D Arrays

---
+++ Strings/StringOperations.java
@@ -0,0 +1,27 @@
+package Strings;
+
+public class StringOperations {
+    public static void main(String[] args) {
+        //concatenation
+        String firstName="tony";
+        String lastName="stark";
+        String fullName=firstName+"@"+lastName;
+        System.out.println(fullName);
+        //tony@stark
+        //length
+        System.out.println("length:"+fullName.length());
+        //charAt
+        for(int i=0;i<fullName.length();i++){
+            System.out.print(fullName.charAt(i));
+        }
+        System.out.println();
+        //compare
+        String name1="tony";
+        String name2="tony";
+        if(name1.compareTo(name2)==0){
+            System.out.println("Strings are equal");
+        }else {
+            System.out.println("Strings are not equal");
+        }
+    }
+}
