a95d92b30b3c49e59db56c7320d943fe43ca1cfa
Ullasachar16 <ullasachar16@gmail.com>
2022-12-05 11:04:49 +0530

    1

---
+++ BasicFundamentals/NestedIf.java
@@ -0,0 +1,17 @@
+package BasicFundamentals;
+
+public class NestedIf {
+    public static void main(String[] args) {
+        String name="john";
+        String password="john404";
+        if(name=="john"){
+            if(password=="john404"){
+                System.out.println("login successful");
+            }else {
+                System.out.println("invalid password");
+            }
+        }else{
+            System.out.println("invalid credentials");
+        }
+    }
+}
