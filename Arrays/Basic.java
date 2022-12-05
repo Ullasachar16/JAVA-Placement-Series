d9819564e87dfee504f9c3a758f1a91b4e676b21
Ullasachar16 <ullasachar16@gmail.com>
2022-12-05 12:39:33 +0530

    Arrays

---
+++ Arrays/Basic.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+public class Basic {
+    public static void main(String[] args) {
+        int marks[]={97,98,99};
+//        int[] marks = new int[3];
+//        marks[0] = 97;
+//        marks[1] = 98;
+//        marks[2] = 99;
+        System.out.println(marks[0]);
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);
+        //inside loop
+        System.out.println("Inside loop");
+        for(int i=0;i<3;i++){
+            System.out.println(marks[i]);
+        }
+    }
+
+}
---
+++ Arrays/Basic1.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class Basic1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter array size:");
+        int size=sc.nextInt();
+        int numbers[]=new int[size];
+        System.out.println("Enter array elements:");
+        for(int i=0;i<size;i++){
+            numbers[i]= sc.nextInt();
+        }
+        System.out.println("Array Elements are:");
+        for(int i=0;i<size;i++){
+            System.out.print(numbers[i]+" ");
+        }
+    }
+}
