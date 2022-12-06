7afe30e525637c87bb822f3943f9b14a587cc3f7
Ullasachar16 <ullasachar16@gmail.com>
2022-12-06 19:32:39 +0530

    String Builder

---
+++ StringBuilder/BasicOperations.java
@@ -0,0 +1,31 @@
+package StringBuilder;
+
+public class BasicOperations {
+    public static void main(String[] args) {
+        StringBuilder sb=new StringBuilder("Tonk");
+        System.out.println(sb);
+
+        //char at index 0
+        System.out.println(sb.charAt(0));
+
+        //set char at index 0
+        sb.setCharAt(0,'p');
+        System.out.println(sb);
+
+        //insert
+        sb.insert(2,'n');
+        System.out.println(sb);
+
+        //delete
+        sb.delete(2,4);
+        System.out.println(sb);
+
+        //append
+        sb.append("e");
+        sb.append("r");
+        System.out.println(sb);
+        //length
+        System.out.println(sb.length());
+
+    }
+}
---
+++ Strings/ReverseAString.java
@@ -0,0 +1,12 @@
+package Strings;
+
+public class ReverseAString {
+    public static void main(String[] args) {
+        String s="Aman";
+        String s1="";
+        for(int i=s.length()-1;i>=0;i--){
+            s1=s1+s.charAt(i);
+        }
+        System.out.println(s1);
+    }
+}
