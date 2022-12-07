cc1be2efa61ebef9ed1bd8ee1a86c516b67f4b62
Ullasachar16 <ullasachar16@gmail.com>
2022-12-07 14:15:58 +0530

    String Builder

---
+++ StringBuilder/CumulativeLengthOfStrings.java
@@ -0,0 +1,20 @@
+package StringBuilder;
+
+import java.util.Scanner;
+
+public class CumulativeLengthOfStrings {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter your string value: ");
+        int size=sc.nextInt();
+        StringBuilder array[]=new StringBuilder[size];
+        int totLength=0;
+        for(int i=0;i<size;i++){
+            array[i]=sb.append(sc.next());
+            totLength = array[i].length();
+        }
+        //returning total input character
+        System.out.println("total length: "+totLength);
+    }
+}
