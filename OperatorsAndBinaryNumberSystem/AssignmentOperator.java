4d8c3c1448348b228ceef3ebd96d6ef0096501c1
Ullasachar16 <ullasachar16@gmail.com>
2022-12-07 15:03:14 +0530

    Operators Basic

---
+++ OperatorsAndBinaryNumberSystem/AssignmentOperator.java
@@ -0,0 +1,18 @@
+package OperatorsAndBinaryNumberSystem;
+
+public class AssignmentOperator {
+    public static void main(String[] args) {
+        //addition
+        int a=10,b=5;
+        System.out.println(a+b);
+        //subtraction
+        System.out.println(a-b);
+        //multiply
+        System.out.println(a*b);
+        //divide
+        System.out.println(a/b);
+        //mod
+        System.out.println(a%b);
+
+    }
+}
