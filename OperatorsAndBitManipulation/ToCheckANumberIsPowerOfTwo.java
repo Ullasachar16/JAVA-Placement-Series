2b5faadaeeb444aadf0bd4f9b37976ac6aa0fc8a
Ullasachar16 <ullasachar16@gmail.com>
2022-12-08 14:12:53 +0530

    Bit Manipulation

---
+++ OperatorsAndBitManipulation/ToCheckANumberIsPowerOfTwo.java
@@ -0,0 +1,20 @@
+package OperatorsAndBitManipulation;
+
+public class ToCheckANumberIsPowerOfTwo {
+    static boolean isPowerOfTwo(int x) {
+        if (x == 0)
+            return false;
+        while (x != 1) {
+            if (x % 2 != 0)
+                return false;
+            x = x / 2;
+        }
+        return true;
+    }
+    // Driver method
+    public static void main(String[] args)
+    {
+        System.out.println(isPowerOfTwo(32) ? "Yes" : "No");
+        System.out.println(isPowerOfTwo(63) ? "Yes" : "No");
+    }
+}
