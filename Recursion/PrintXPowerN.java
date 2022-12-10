3a49652e9a41d1faf3e5bbcc430825e1a0c14a17
Ullasachar16 <ullasachar16@gmail.com>
2022-12-10 13:20:22 +0530

    Recursion

---
+++ Recursion/PrintXPowerN.java
@@ -0,0 +1,20 @@
+package Recursion;
+
+public class PrintXPowerN {
+    //stack height = n
+    public static int calcPower(int x,int n){
+        if(n==0){ //base case 1
+            return 1;
+        }
+        if(x==0){ //base case 2
+            return 0;
+        }
+        int xPownm1=calcPower(x,n-1);
+        int xPown=x*xPownm1;
+        return xPown;
+    }
+    public static void main(String[] args) {
+        int x=2,n=5;
+        System.out.println(calcPower(x,n));
+    }
+}
---
+++ Recursion/PrintXPowerN_1.java
@@ -0,0 +1,7 @@
+package Recursion;
+
+public class PrintXPowerN_1 {
+    public static void main(String[] args) {
+
+    }
+}
