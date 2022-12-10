0f36cff36dd14493260cbe4bccc2196aab081099
Ullasachar16 <ullasachar16@gmail.com>
2022-12-10 12:38:57 +0530

    Recursion

---
+++ Recursion/PrintNumbers.java
@@ -0,0 +1,26 @@
+package Recursion;
+
+public class PrintNumbers {
+    //print numbers from 5 to 1
+    public static void printNumber(int n){
+        if(n==0)
+            return;
+        System.out.print(n);
+        printNumber(n-1);
+    }
+    public static void main(String[] args) {
+        int n=5;
+        printNumber(n);
+        System.out.println();
+        n=1;
+        printNumbers(n);
+
+    }
+    //print numbers from 1 to 5
+    public static void printNumbers(int n){
+        if(n==6)
+            return;
+        System.out.print(n);
+        printNumbers(n+1);
+    }
+}
