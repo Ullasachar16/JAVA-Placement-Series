3fb74c55badc45e871f7f3ade0510a5792feb89a
Ullasachar16 <ullasachar16@gmail.com>
2022-12-10 14:25:26 +0530

    Recursion Continued

---
+++ Recursion/FindingFirstAndLastOccurrenceOfElementInAString.java
@@ -0,0 +1,28 @@
+package Recursion;
+
+public class FindingFirstAndLastOccurrenceOfElementInAString {
+    public static int first=-1;
+    public static int last=-1;
+
+    public static void findOccurrence(String str,int index,char element){
+
+        if(index==str.length()){
+            System.out.println(first);
+            System.out.println(last);
+            return;
+        }
+        char currentChar=str.charAt(index);
+        if(currentChar==element){
+            if(first==-1){
+                first=index;
+            }else {
+                last=index;
+            }
+        }
+        findOccurrence(str,index+1,element);
+    }
+    public static void main(String[] args) {
+        String str="abaacdaefaah";
+        findOccurrence(str,0,'a');
+    }
+}
