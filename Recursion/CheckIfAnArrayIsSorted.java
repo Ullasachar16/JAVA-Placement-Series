38c36d8034df91cb8d388805bf84903534288b94
Ullasachar16 <ullasachar16@gmail.com>
2022-12-10 14:33:45 +0530

    Recursion Continued

---
+++ Recursion/CheckIfAnArrayIsSorted.java
@@ -0,0 +1,22 @@
+package Recursion;
+
+public class CheckIfAnArrayIsSorted {
+
+    //check if an array is sorted(Strictly Increasing)
+    public static boolean isSorted(int arr[],int index){
+        if(index== arr.length-1){
+            return true;
+        }
+        if(arr[index]>=arr[index+1]){
+            //array is unsorted
+            return false;
+        }else {
+            return isSorted(arr,index+1);
+        }
+    }
+    public static void main(String[] args) {
+        int arr[]={1,3,3};
+        System.out.println(isSorted(arr,0));
+
+    }
+}
