9583d874dae83e482c816e40cae27bbfde3509cf
Ullasachar16 <ullasachar16@gmail.com>
2022-12-10 13:29:34 +0530

    Recursion

--- Recursion/PrintXPowerN_1.java
+++ Recursion/PrintXPowerN_1.java
@@ -1,7 +1,23 @@
 package Recursion;
 
 public class PrintXPowerN_1 {
+    //stack height = log n
+    public static int calcPower(int x,int n){
+        if(n==0){ //base case 1
+            return 1;
+        }
+        if(x==0){ //base case 2
+            return 0;
+        }
+        //if n is even
+        if(n%2==0){
+            return calcPower(x,n/2)*calcPower(x,n/2);
+        }else { //if n is odd
+            return calcPower(x,n/2)*calcPower(x,n/2)*x;
+        }
+    }
     public static void main(String[] args) {
-
+        int x=2,n=5;
+        System.out.println(calcPower(x,n));
     }
 }
