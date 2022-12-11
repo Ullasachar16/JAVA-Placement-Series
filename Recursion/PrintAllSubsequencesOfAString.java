8ed3406ce19e4835a26ac9374e63c697d5705a3f
Ullasachar16 <ullasachar16@gmail.com>
2022-12-11 12:39:38 +0530

    Recursion Continued

--- Recursion/PrintAllSubsequencesOfAString.java
+++ Recursion/PrintAllSubsequencesOfAString.java
@@ -1,7 +1,20 @@
 package Recursion;
 
 public class PrintAllSubsequencesOfAString {
+    public static void subsequences(String str,int index,String newString){
+        if(index==str.length()){
+            System.out.println(newString);
+            return;
+        }
+        char currChar=str.charAt(index);
+        //to be
+        subsequences(str,index+1,newString+currChar);
+        //to not to be
+        subsequences(str,index+1,newString);
+    }
     public static void main(String[] args) {
+        String str="abc";
+        subsequences(str,0,"");
 
     }
 }
