6c66e9561698bc69cfa6479aeb0f343877d0ab23
Ullasachar16 <ullasachar16@gmail.com>
2022-12-11 12:22:52 +0530

    Recursion Continued

--- Recursion/RemoveDuplicatesInAString.java
+++ Recursion/RemoveDuplicatesInAString.java
@@ -1,7 +1,24 @@
 package Recursion;
 
 public class RemoveDuplicatesInAString {
+    public static boolean[] map=new boolean[26];
+    public static void removeDuplicates(String str,int index,String newString){
+        if(index == str.length()){
+            System.out.println(newString);
+            return;
+        }
+        char currChar=str.charAt(index);
+        if(map[currChar - 'a']){
+            removeDuplicates(str,index+1,newString);
+        }else{
+            newString+=currChar;
+            map[currChar - 'a']=true;
+            removeDuplicates(str,index+1,newString);
+        }
+    }
     public static void main(String[] args) {
+        String str="abbccda";
+        removeDuplicates(str,0,"");
 
     }
 }
