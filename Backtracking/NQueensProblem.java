e436abce91fc264d49486b0f7accc32ac1565350
Ullasachar16 <ullasachar16@gmail.com>
2022-12-12 13:38:56 +0530

    Backtracking

--- Backtracking/NQueensProblem.java
+++ Backtracking/NQueensProblem.java
@@ -13,8 +13,53 @@ public class NQueensProblem {
         }
         //vertical
         for(int i=0;i<board.length;i++){
-
+            if(board[i][col]=='Q'){
+                return false;
+            }
         }
+        //upper left
+        int r=row;
+        for(int c=col;c>=0 && r>=0;c--,r--){
+            if(board[r][c]=='Q'){
+                return false;
+            }
+        }
+        //upper right
+        r=row;
+        for(int c=col;c<board.length && r>=0;r--,c++){
+            if(board[r][c]=='Q'){
+                return false;
+            }
+        }
+        //lower left
+        r=row;
+        for(int c=col;c>=0 && r<board.length;r++,c--){
+            if(board[r][c]=='Q'){
+                return false;
+            }
+        }
+        //lower right
+        for(int c=col; c< board.length && r< board.length;c++,r++){
+            if(board[r][c]=='Q'){
+                return false;
+            }
+        }
+        return true;
+    }
+    public void saveBoard(char[][] board,List<List<String>> allBoards){
+        String row="";
+        List<String> newBoard=new ArrayList<>();
+        for(int i=0;i<board.length;i++){
+            row="";
+            for(int j=0;j<board[0].length;j++){
+                if(board[i][j]=='Q')
+                    row +='Q';
+                else
+                    row+='.';
+            }
+            newBoard.add(row);
+        }
+        allBoards.add(newBoard);
     }
     public void helper(char[][] board,List<List<String>> allBoards,int col){
         if(col==board.length){
@@ -25,15 +70,13 @@ public class NQueensProblem {
             if(isSafe(row,col,board)){
                 board[row][col] = 'Q';
                 helper(board,allBoards,col+1);
-                board[row][col] = ".";
+                board[row][col] = '.';
             }
         }
     }
     public List<List<String>> solveNQueens(int n){
         List<List<String>> allBoards=new ArrayList<>();
-        char[][] board = new board[n][n];
-    }
-    public static void main(String[] args) {
-
+        char[][] board = new char[n][n];
+        helper(board,allBoards,0);
     }
 }
