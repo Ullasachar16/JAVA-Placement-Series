ed2d8f8fcf12b5fa7efb0e588385c264e9285d56
Ullasachar16 <ullasachar16@gmail.com>
2022-12-12 13:07:03 +0530

    Backtracking

--- Backtracking/PermutationProblem.java
+++ Backtracking/PermutationProblem.java
@@ -1,4 +1,20 @@
 package Backtracking;
 
 public class PermutationProblem {
+    public static void printPermutation(String str,String perm,int index){
+        if(str.length() == 0){
+            System.out.println(perm);
+            return;
+        }
+        for (int i=0;i<str.length();i++){
+            char currChar=str.charAt(i);
+            String newStr=str.substring(0,i)+str.substring(i+1);
+            printPermutation(newStr,perm+currChar,index+1);
+        }
+    }
+    public static void main(String[] args) {
+        String str="abc";
+        printPermutation(str," ",0);
+    }
+
 }
