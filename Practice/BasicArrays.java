1accc2ea5791af73891b361559792b1b74de296e
Ullasachar16 <ullasachar16@gmail.com>
2022-12-13 13:58:12 +0530

    Commit

--- Practice/BasicArrays.java
+++ Practice/BasicArrays.java
@@ -1,20 +1,22 @@
 package Practice;
 
-public class BasicArrays {
-    public static void main(String[] args) {
-//        int[] myIntArray={1,2,3,4,5,6,7,8,9,10};
-//        System.out.println(myIntArray[4]);
-//        System.out.println(myIntArray[0]);
-        int[] myIntArray=new int[20];
-        for(int i=0;i< myIntArray.length;i++){
-            myIntArray[i]=i*10;
-        }
-        printArray(myIntArray);
+import java.util.Scanner;
 
-    }
-    public static void printArray(int[] array){
-        for(int i=0;i< array.length;i++){
-            System.out.println("Element " + i +" ,value is " +array[i]);
+public class BasicArrays {
+    private static Scanner sc=new Scanner(System.in);
+    public static void main(String[] args) {
+        int[] arr=getIntegers(5);
+        for(int i=0;i<arr.length;i++){
+            System.out.println("Element "+i+" , typed value was "+arr[i]);
         }
     }
+    public static int[] getIntegers(int number){
+        System.out.println("Enter "+number+ " integer value");
+        int[] values=new int[number];
+        for(int i=0;i<values.length;i++){
+            values[i]=sc.nextInt();
+        }
+        return values;
+    }
+
 }
