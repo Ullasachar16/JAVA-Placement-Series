0a781261a2916f74d84bf4afaefb66a0eb0ad8bd
Ullasachar16 <ullasachar16@gmail.com>
2022-12-13 12:43:01 +0530

    Sorting

--- Sorting/MergeSort.java
+++ Sorting/MergeSort.java
@@ -2,10 +2,12 @@ package Sorting;
 
 public class MergeSort {
     public static void conquer(int arr[],int si,int mid,int ei){
+        //n log n
         int merged[]=new int[ei-si+1];
         int idx1=si;
         int idx2=mid+1;
         int x=0;
+        //O(n)
         while(idx1<=mid && idx2<=ei){
             if(arr[idx1] <= arr[idx2]){
                 merged[x++]=arr[idx1++];
@@ -27,6 +29,7 @@ public class MergeSort {
         if(si>=ei){
             return;
         }
+        //O(log n)
         int mid=si+(ei-si)/2;
         divide(arr,si,mid);
         divide(arr,mid+1,ei);
