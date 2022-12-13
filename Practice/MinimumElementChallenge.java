f1d9055915c16c132a578cc2e0a311325f9f9ebc
Ullasachar16 <ullasachar16@gmail.com>
2022-12-13 14:53:12 +0530

    Commit

--- Practice/MinimumElementChallenge.java
+++ Practice/MinimumElementChallenge.java
@@ -14,9 +14,15 @@ public class MinimumElementChallenge {
         }
         return array;
     }
-    private static int[] findMin(int array){
+    private static int findMin(int[] array){
         int min=Integer.MAX_VALUE;
-
+        for(int i=0;i<array.length;i++){
+            int value=array[i];
+            if(value<min){
+                min=value;
+            }
+        }
+        return min;
     }
     public static void main(String[] args) {
         System.out.println("Enter count:");
@@ -24,6 +30,7 @@ public class MinimumElementChallenge {
         sc.nextLine();
 
         int[] returnedArray=readIntegers(count);
-
+        int returnedMin=findMin(returnedArray);
+        System.out.println("Min: "+returnedMin);
     }
 }
