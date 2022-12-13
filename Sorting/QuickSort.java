19f6afaaf64578695875b26fc8b18ff30ae8e49e
Ullasachar16 <ullasachar16@gmail.com>
2022-12-13 13:22:05 +0530

    Sorting

--- Sorting/QuickSort.java
+++ Sorting/QuickSort.java
@@ -1,7 +1,30 @@
 package Sorting;
 
 public class QuickSort {
-    public static void main(String[] args) {
+    public static int partition(int arr[],int low,int high){
+        int pivot=arr[high];
+        int i = low-1;
+        for(int j=low;j<high;j++){
+            if(arr[j] < pivot){
+                i++;
+                //swap
+            }else{
 
+            }
+        }
+    }
+    public static void quickSort(int arr[],int low,int high){
+        if(low<high){
+            int pidx=partition(arr,low,high);
+
+            quickSort(arr,low,pidx-1);
+            quickSort(arr,pidx+1,high);
+        }
+    }
+    public static void main(String[] args) {
+        int arr[]={6,3,9,5,2,8};
+        int n= arr.length;
+
+        quickSort(arr,0,n-1);
     }
 }
