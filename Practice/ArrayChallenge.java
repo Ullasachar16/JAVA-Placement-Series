a77d2668af1987273a11b58f6615187e5d663b40
Ullasachar16 <ullasachar16@gmail.com>
2022-12-13 14:25:43 +0530

    Commit

---
+++ Practice/ArrayChallenge.java
@@ -0,0 +1,48 @@
+package Practice;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArrayChallenge {
+    private static Scanner sc=new Scanner(System.in);
+    public static void main(String[] args) {
+        int[] myIntegers=getIntegers(5);
+        int[] sorted=sortArray(myIntegers);
+        printArray(sorted);
+
+    }
+    public static int[] getIntegers(int capacity){
+        System.out.println("Enter "+capacity+ " integer values");
+        int[] array=new int[capacity];
+        for(int i=0;i<array.length;i++){
+            array[i]=sc.nextInt();
+        }
+        return array;
+    }
+    public static void printArray(int[] array){
+        for(int i=0;i<array.length;i++){
+            System.out.println("Element "+i+" contents "+array[i]);
+        }
+    }
+    public static int[] sortArray(int[] array){
+//        int[] sortedArray=new int[array.length];
+//        for(int i=0;i<array.length;i++){
+//            sortedArray[i]=array[i];
+//        }
+        int[] sortedArray= Arrays.copyOf(array,array.length);
+        boolean flag=true;
+        int temp;
+        while(flag){
+            flag=false;
+            for(int i=0;i< sortedArray.length-1;i++){
+                if(sortedArray[i]<sortedArray[i+1]){
+                    temp=sortedArray[i];
+                    sortedArray[i]=sortedArray[i+1];
+                    sortedArray[i+1]=temp;
+                    flag=true;
+                }
+            }
+        }
+        return sortedArray;
+    }
+}
