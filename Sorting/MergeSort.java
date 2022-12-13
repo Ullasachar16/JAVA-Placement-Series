271ea8aee1266b83a4bc8c3f5a7c7c2d9e812f63
Ullasachar16 <ullasachar16@gmail.com>
2022-12-13 13:00:30 +0530

    Sorting

--- Sorting/MergeSort.java
+++ Sorting/MergeSort.java
@@ -42,7 +42,6 @@ public class MergeSort {
         //print
         for(int i=0;i<n;i++){
             System.out.print(arr[i]+" ");
-        }
-        System.out.println();
+        }   System.out.println();
     }
 }
