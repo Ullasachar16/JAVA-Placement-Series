0790c6b9adfda2e035d273399a53c304e5403e2d
Ullasachar16 <ullasachar16@gmail.com>
2022-12-14 14:47:02 +0530

    Commit

---
+++ Practice/BasicTernaryOperator.java
@@ -0,0 +1,10 @@
+package Practice;
+
+public class BasicTernaryOperator {
+    public static void main(String[] args) {
+        int a=80;
+        int b=40;
+        int min=a<b?a:b;
+        System.out.println("min of "+a+ " and " +b+" is: "+min);
+    }
+}
