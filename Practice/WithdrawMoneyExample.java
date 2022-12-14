449ababe1f09fad63379a2200f737df663561728
Ullasachar16 <ullasachar16@gmail.com>
2022-12-14 15:01:07 +0530

    Commit

---
+++ Practice/WithdrawMoneyExample.java
@@ -0,0 +1,14 @@
+package Practice;
+
+public class WithdrawMoneyExample {
+    public static void main(String[] args) {
+        double act_bal=11999;
+        double withdraw=2000;
+        while(act_bal>=2000){
+            System.out.println("Withdraw amount:"+withdraw);
+            act_bal=act_bal-withdraw;
+            System.out.println("account balance:"+act_bal);
+        }
+
+    }
+}
