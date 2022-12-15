daf3f640d899d428ca1aca05572912bb93330c60
Ullasachar16 <ullasachar16@gmail.com>
2022-12-15 20:21:09 +0530

    Commit

---
+++ Recursion/HCFAndLCM.java
@@ -0,0 +1,23 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class HCFAndLCM {
+    public static void main(String[] args) {
+        System.out.println("Enter 2 numbers:");
+        Scanner sc=new Scanner(System.in);
+        int m= sc.nextInt();
+        int n= sc.nextInt();
+
+        int h=1;
+        int p=m*n;
+
+        for(int i=2;i<p;i++){
+            if((m%i==0)&&(n%i==0)){
+                h=i;
+            }
+        }
+        int i=p/h;
+        System.out.println("HCF is:"+h+" and LCM is:"+i);
+    }
+}
