6a6b2a48bc11b03bfdc3f753314388feb416a54c
Ullasachar16 <ullasachar16@gmail.com>
2022-12-15 19:14:59 +0530

    Commit

---
+++ Strings/ReverseASentence.java
@@ -0,0 +1,23 @@
+package Strings;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class ReverseASentence {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Sentence:");
+        String str = sc.nextLine();
+        String rev = "";
+
+        String words[] = str.split(" ");
+
+        //Adding the words stored in the array to the last
+        System.out.println("Reverse Sentence:");
+        for(int i= words.length-1;i>=0;i--){
+            System.out.print(words[i]+" ");
+        }
+        }
+}
+
---
+++ Strings/StringSortingInAlphabeticalOrder.java
@@ -0,0 +1,7 @@
+package Strings;
+
+public class StringSortingInAlphabeticalOrder {
+    public static void main(String[] args) {
+
+    }
+}
