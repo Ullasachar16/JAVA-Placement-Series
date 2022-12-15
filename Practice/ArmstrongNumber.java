4c66457575d648c8cc7a3cedba250720bdc66b50
Ullasachar16 <ullasachar16@gmail.com>
2022-12-15 19:55:39 +0530

    Commit

---
+++ Practice/ArmstrongNumber.java
@@ -0,0 +1,22 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        int n,sum=0,temp,r;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number to check if it is an armstrong number");
+        n=sc.nextInt();
+        temp=n;
+        while(temp!=0){
+            r=temp%10;
+            sum=sum+r*r*r;
+            temp=temp/10;
+        }
+        if(n==sum)
+            System.out.println("Entered number is an armstrong number");
+        else
+            System.out.println("Entered number is not an armstrong number");
+    }
+}
