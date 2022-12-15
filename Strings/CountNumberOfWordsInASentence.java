654cf132e92206d873966a30b5a3148a8807c3ca
Ullasachar16 <ullasachar16@gmail.com>
2022-12-15 19:03:04 +0530

    Commit

---
+++ Strings/CountNumberOfWordsInASentence.java
@@ -0,0 +1,18 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class CountNumberOfWordsInASentence {
+    public static int countWords(String sentence){
+        String words[]=sentence.split("");
+        int count = words.length;
+        return count;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a sentence:");
+        String sentence=sc.nextLine();
+        System.out.println("Your sentence has "+countWords(sentence)+ " words");
+        sc.close();
+    }
+}
