c2404eee535907301e6deb6b9e4e3ea2c4301a22
Ullasachar16 <ullasachar16@gmail.com>
2022-12-15 20:11:33 +0530

    Commit

---
+++ Recursion/FibonacciSeries1.java
@@ -0,0 +1,23 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class FibonacciSeries1 {
+    public static int fibo(int n){
+        if(n==0)
+            return 1;
+        if(n==1)
+            return 1;
+        else
+            return fibo(n-1)+fibo(n-2);
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Fibonacci Term:");
+        int n=sc.nextInt();
+        System.out.print("Fibonacci Series is:\n");
+        for(int i=0;i<n;i++){
+            System.out.print(fibo(i)+" ");
+        }
+    }
+}
