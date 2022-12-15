68e68f56fe4f07cef73d4847e8f4b5c4ba4a5624
Ullasachar16 <ullasachar16@gmail.com>
2022-12-15 20:06:30 +0530

    Commit

---
+++ Recursion/Factorial1.java
@@ -0,0 +1,19 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class Factorial1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number to know its factorial:");
+        int a= sc.nextInt();
+        System.out.println("Factorial of "+a+" is "+fact(a));
+    }
+    public static int fact(int n){
+        int result;
+        if(n==0||n==1)
+            return 1;
+        result=fact(n-1)*n;
+        return result;
+    }
+}
--- Recursion/PrintFactorialOfNumberN.java
+++ Recursion/PrintFactorialOfNumberN.java
@@ -2,14 +2,14 @@ package Recursion;
 
 public class PrintFactorialOfNumberN {
     public static int printFactorial(int n){
-        if(n==1)
+        if(n==1||n==0)
             return 1;
         int fact_nm1=printFactorial(n-1);
         int fact_n=n*fact_nm1;
         return fact_n;
     }
     public static void main(String[] args) {
-        int n=5;
+        int n=4;
         System.out.println("Factorial:"+printFactorial(n));
     }
 }
