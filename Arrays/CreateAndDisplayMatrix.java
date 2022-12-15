a9a9fb9e9853864c5185e7637f83d1458a2a708d
Ullasachar16 <ullasachar16@gmail.com>
2022-12-15 19:33:56 +0530

    Commit

---
+++ Arrays/CreateAndDisplayMatrix.java
@@ -0,0 +1,44 @@
+package Arrays;
+
+import java.util.Scanner;
+
+class Matrix_Create{
+    Scanner scan;
+    int matrix[][];
+    int row,column;
+
+    void create(){
+        scan =new Scanner(System.in);
+        System.out.println("Matrix Creation");
+        System.out.println("Enter no of rows");
+        row=Integer.parseInt(scan.nextLine());
+        System.out.println("Enter no of columns");
+        column = Integer.parseInt(scan.nextLine());
+
+        matrix=new int[row][column];
+        System.out.println("Enter the data");
+        for(int i=0;i<row;i++){
+            for(int j=0;j<column;j++){
+                matrix[i][j]=scan.nextInt();
+            }
+        }
+            }
+    void display(){
+        System.out.println("\nThe Matrix is:");
+        for(int i=0;i<row;i++){
+            for(int j=0;j<column;j++){
+                System.out.print("\t"+matrix[i][j]);
+            }
+            System.out.println();
+        }
+
+    }
+}
+public class CreateAndDisplayMatrix {
+    public static void main(String[] args) {
+        Matrix_Create obj=new Matrix_Create();
+        obj.create();
+        obj.display();
+    }
+
+}
