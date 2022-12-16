754553d0c8f5decdb95abaf51aaee510d1d8989b
Ullasachar16 <ullasachar16@gmail.com>
2022-12-16 13:47:01 +0530

    Commit

---
+++ OOPSPractice/ArrayEx1.java
@@ -0,0 +1,22 @@
+package OOPSPractice;
+
+public class ArrayEx1 {
+    int age;
+    String name;
+
+    ArrayEx1(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+}
+class solut{
+    public static void main(String[] args) {
+        ArrayEx1 ax=new ArrayEx1(45,"tom");
+        ArrayEx1 ax1=new ArrayEx1(54,"jack");
+        ArrayEx1[] s={ax,ax1};
+        for(int i=0;i< s.length;i++){
+            System.out.println(s[i].age+" "+s[i].name);
+        }
+
+    }
+}
