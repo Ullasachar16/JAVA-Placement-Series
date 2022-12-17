f1842e696ea36544c37a34843292cc28bcbd2a3f
Ullasachar16 <ullasachar16@gmail.com>
2022-12-17 14:59:12 +0530

    Commit

---
+++ Practice/StudentPractice.java
@@ -0,0 +1,26 @@
+package Practice;
+
+public class StudentPractice {
+    int age;
+    String name;
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public String getName() {
+        return name;
+    }
+}
+class Soluto{
+    public static void main(String[] args) {
+        StudentPractice s1=new StudentPractice();
+        s1.setAge(35);
+        s1.setName("Tom");
+        System.out.println("id:"+s1.age+"\nname:"+s1.name);
+    }
+}
