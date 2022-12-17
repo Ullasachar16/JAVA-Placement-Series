e6393b4fbb5b078445eb023395f31dc44cf4bb23
Ullasachar16 <ullasachar16@gmail.com>
2022-12-17 19:30:49 +0530

    Commit

---
+++ OOPS/Solution1.java
@@ -0,0 +1,17 @@
+package OOPS;
+
+public class Solution1{
+    static void display(Father3 obj){
+        if(obj instanceof Son3){
+            Son3 s=(Son3) obj;
+            System.out.println(s.x+" "+s.y);
+        }else if(obj instanceof Daughter){
+            Daughter d=(Daughter) obj;
+            System.out.println(d.x+" "+d.z);
+        }
+    }
+    public static void main(String[] args) {
+        display(new Son3());
+        display(new Daughter());
+    }
+}
