3566a07503585c6d21afd7c800fee9cdd2866b57
Ullasachar16 <ullasachar16@gmail.com>
2022-12-17 18:31:11 +0530

    Commit

---
+++ OOPS/PrimitiveCasting.java
@@ -0,0 +1,31 @@
+package OOPS;
+
+public class PrimitiveCasting {
+    public static void main(String[] args) {
+        //Widening
+        int a=45;
+        float f=a;
+        System.out.println(f);
+
+        float f1=4.55f;
+        double d=f1;
+        System.out.println(d);
+
+        char x='a';
+        int b=x;
+        System.out.println(b);
+
+        //Narrowing
+        f1=4.55f;
+        int a1=(int)f1;
+        System.out.println(a1);
+
+        double d1=3.15;
+        int b1=(int)d1;
+        System.out.println(b1);
+
+        int x1=97;
+        char y=(char)x1;
+        System.out.println(y);
+    }
+}
