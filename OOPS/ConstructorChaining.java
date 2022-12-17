24e20fa53fa75cc2789e96ac7e7c7f5b47a3bf5e
Ullasachar16 <ullasachar16@gmail.com>
2022-12-17 12:40:12 +0530

    Commit

---
+++ OOPS/ConstructorChaining.java
@@ -0,0 +1,22 @@
+package OOPS;
+
+public class ConstructorChaining {
+
+}
+class Student{
+    Student(int age){
+        this(5.7);
+        System.out.println("age:"+age);
+    }
+    Student(String name){
+        this(20);
+        System.out.println("name:"+name);
+    }
+    Student(double height){
+        System.out.println("height:"+height);
+    }
+
+    public static void main(String[] args) {
+        Student s=new Student("tom");
+    }
+}
