7c2fd9a224c78056567947fae716130efd19d9d3
Ullasachar16 <ullasachar16@gmail.com>
2022-12-17 12:59:59 +0530

    Commit

---
+++ OOPS/ConstructorChainingAdvanced.java
@@ -0,0 +1,27 @@
+package OOPS;
+
+public class ConstructorChainingAdvanced {
+    public static void main(String[] args) {
+        B b=new B(5.7);
+    }
+}
+class A{
+    A(int x){
+        this("tom");
+        System.out.println(1);
+    }
+    A(String x){
+        System.out.println(2);
+    }
+}
+class B extends A{
+    B(){
+        super(10);
+        System.out.println(3);
+    }
+    B(double x){
+        this();
+        System.out.println(4);
+    }
+}
+
