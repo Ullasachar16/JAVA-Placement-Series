6f401ba30f05696eae86cd28d268b73a788d91ad
Ullasachar16 <ullasachar16@gmail.com>
2022-12-17 19:39:21 +0530

    Commit

---
+++ OOPS/Employ.java
@@ -0,0 +1,33 @@
+package OOPS;
+
+public class Employ {
+    String Company="Amazon";
+}
+class Tester1 extends Employ{
+    void testApp(){
+        System.out.println("Testing app");
+    }
+}
+class Developer2 extends Employ{
+    void developApp(){
+        System.out.println("Developing app");
+    }
+}
+class Solution2{
+        Employ e=new Developer2(); //or new Tester1();
+        static void work(Employ e) {
+            if (e instanceof Developer2) {
+                Developer2 d = (Developer2) e;
+                System.out.println("Company name:" + d.Company);
+                d.developApp();
+            } else if (e instanceof Tester1) {
+                Tester1 t = (Tester1) e;
+                System.out.println("Company name:" + t.Company);
+                t.testApp();
+            }
+        }
+    public static void main(String[] args) {
+            work(new Developer2());
+            work(new Tester1());
+    }
+    }
