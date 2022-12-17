57889e15651101e90dd64f5a4c7c9de2f1a3ea6e
Ullasachar16 <ullasachar16@gmail.com>
2022-12-17 13:42:28 +0530

    Commit

---
+++ OOPS/AccessingPublicMembers.java
@@ -0,0 +1,26 @@
+package OOPS;
+
+public class AccessingPublicMembers {
+    //accessing public members in diff class
+    public static void main(String[] args) {
+        Student2 s1=new Student2();
+        System.out.println(s1.age);
+        s1.study();
+    }
+}
+class Student2{
+    //accessing public members with same class
+    public int age=25;
+    public Student2(){
+        System.out.println("In Student");
+    }
+    public void study(){
+        System.out.println("Studying");
+    }
+
+//    public static void main(String[] args) {
+//        Student2 s=new Student2();
+//        System.out.println("Age:"+s.age);
+//        s.study();
+//    }
+}
