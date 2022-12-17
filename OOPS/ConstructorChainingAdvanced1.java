aee98a809793177baed2f3beceee452a6f754a5a
Ullasachar16 <ullasachar16@gmail.com>
2022-12-17 13:16:47 +0530

    Commit

---
+++ OOPS/ConstructorChainingAdvanced1.java
@@ -0,0 +1,48 @@
+package OOPS;
+
+public class ConstructorChainingAdvanced1 {
+    public static void main(String[] args) {
+        Bike1 b=new Bike1("Bajaj",375000,"green");
+        b.display();
+        System.out.println("+++++++++++++++++++++++");
+        Car1 c=new Car1("Suzuki",700000,"Petrol");
+        c.display();
+    }
+}
+class Vehicle1{
+    String brand;
+    int cost;
+    Vehicle1(String brand,int cost){
+        this.brand=brand;
+        this.cost=cost;
+    }
+    void display(){
+        System.out.println("Brand:"+brand);
+        System.out.println("Cost:"+cost);
+    }
+}
+class Car1 extends Vehicle1{
+    String fuel;
+    Car1(String brand,int cost,String fuel){
+        super(brand, cost);
+        this.fuel=fuel;
+    }
+    @Override
+    void display(){
+        super.display();
+        System.out.println("Fuel:"+fuel);
+    }
+}
+class Bike1 extends Vehicle1{
+    String color;
+    Bike1(String brand,int cost,String color){
+        super(brand, cost);
+        this.color=color;
+    }
+
+    @Override
+    void display() {
+        super.display();
+        System.out.println("Color:"+color);
+    }
+}
--- OOPS/ConstructorChainingContinued.java
+++ OOPS/ConstructorChainingContinued.java
@@ -1,5 +1,36 @@
 package OOPS;
 
 public class ConstructorChainingContinued {
+    public static void main(String[] args) {
+        Student1 s=new Student1(10,"Jack",95);
+        System.out.println("Student Details");
+        System.out.println("Name:"+s.name+"\nId:"+s.id+"\nMarks:"+s.marks);
+        System.out.println("*********************************");
+        Employee3 e=new Employee3(101,"Tom",45.5);
+        System.out.println("Employee Details");
+        System.out.println("Name:"+e.name+"\nId:"+e.id+"\nSalary:"+e.salary);
+    }
 }
+class Person{
+    int id;
+    String name;
 
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
+class Student1 extends Person{
+    int marks;
+    Student1(int id,String name,int marks){
+        super(id, name);
+        this.marks=marks;
+    }
+}
+class Employee3 extends Person{
+    double salary;
+    Employee3(int id,String name,double salary){
+        super(id, name);
+        this.salary=salary;
+    }
+}
