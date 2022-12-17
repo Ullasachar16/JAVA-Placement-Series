209a044f72e29c122dfb93fa6e84720dd26db185
Ullasachar16 <ullasachar16@gmail.com>
2022-12-17 19:46:59 +0530

    Commit

---
+++ OOPS/User.java
@@ -0,0 +1,37 @@
+package OOPS;
+
+import java.util.Scanner;
+
+class Beverage{
+}
+class Coffee extends Beverage{
+}
+class Tea extends Beverage{
+}
+class Cafe{
+    Beverage vendingMachine(int choice){
+        if(choice==1){
+            return new Coffee();
+        }else if(choice == 2){
+            return new Tea();
+        }else {
+            return null;
+        }
+    }
+}
+public class User {
+    public static void main(String[] args) {
+        Cafe c=new Cafe();
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter 1 for coffee and 2 for Tea");
+        int choice=sc.nextInt();
+        Beverage obj=c.vendingMachine(choice);
+        if(obj instanceof Coffee){
+            System.out.println("Drinking Coffee");
+        }else if(obj instanceof Tea){
+            System.out.println("Drinking Tea");
+        }else{
+            System.out.println("Invalid");
+        }
+    }
+}
