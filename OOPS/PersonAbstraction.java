851e4eba2a63d34ca2003a2810c3c8a2ffd830af
Ullasachar16 <ullasachar16@gmail.com>
2022-12-18 14:23:05 +0530

    Commit

---
+++ OOPS/PersonAbstraction.java
@@ -0,0 +1,18 @@
+package OOPS;
+public abstract class PersonAbstraction{
+    abstract void work();
+}
+class Employe2 extends PersonAbstraction{
+    @Override
+    public void work(){
+        System.out.println("Working");
+    }
+}
+class Test{
+    public static void main(String[] args) {
+        Employe2 e=new Employe2();
+        e.work();       //normal invoke of overridden method
+        PersonAbstraction p=new Employe2(); //Upcasting
+        p.work();       //Abstraction
+    }
+}
