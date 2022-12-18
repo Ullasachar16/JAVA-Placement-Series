3f2b49f90908e18d6629e2263cef91e46014da74
Ullasachar16 <ullasachar16@gmail.com>
2022-12-18 13:38:54 +0530

    Commit

---
+++ OOPS/CompileTimePolymorphism.java
@@ -0,0 +1,25 @@
+package OOPS;
+
+public class CompileTimePolymorphism {
+    public static void main(String[] args) {
+        Flipkart f=new Flipkart();
+        f.purchase("mobile",40000);
+        f.purchase(35000,"water purifier");
+        f.purchase(3000);
+        f.purchase("Smartwatch");
+    }
+}
+class Flipkart{
+    void purchase(int cost){
+        System.out.println("cost:"+cost);
+    }
+    void purchase(String product){
+        System.out.println("product:"+product);
+    }
+    void purchase(String product,int cost){
+        System.out.println("product:"+product+" cost:"+cost);
+    }
+    void purchase(int cost,String product){
+        System.out.println("cost:"+cost+" product:"+product);
+    }
+}
