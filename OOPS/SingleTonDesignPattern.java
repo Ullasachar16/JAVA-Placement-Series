3c4d2c86fb4551ab3e2499b759344f4e7c6e99fd
Ullasachar16 <ullasachar16@gmail.com>
2022-12-18 13:59:42 +0530

    Commit

---
+++ OOPS/SingleTonDesignPattern.java
@@ -0,0 +1,22 @@
+package OOPS;
+
+public class SingleTonDesignPattern {
+    public static void main(String[] args) {
+        Account.createObject();
+        Account.createObject();
+        Account.createObject();
+    }
+}
+class Account{
+    private static Account obj;
+    private Account(){
+        System.out.println("Object Created");
+    }
+    public static void createObject(){
+        if(obj==null){
+            obj=new Account();
+        }else{
+            System.out.println("Cannot create object");
+        }
+    }
+}
