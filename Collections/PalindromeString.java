65f6f93477f1ebb01f2d173357de99ffb86eec32
Ullasachar16 <ullasachar16@gmail.com>
2022-12-19 15:28:32 +0530

    Commit

---
+++ Collections/PalindromeString.java
@@ -0,0 +1,16 @@
+package Collections;
+
+public class PalindromeString {
+    public static void main(String[] args) {
+        String s="madan";
+        String s1="";
+        for(int i=s.length()-1;i>=0;i--){
+            s1=s1+s.charAt(i);
+        }
+        if(s1.equals(s)){
+            System.out.println(s+" is palindrome");
+        }else{
+            System.out.println(s+" is not a palindrome");
+        }
+    }
+}
