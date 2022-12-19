6ff6ad3f7a82a623b5dc68b8aa7b1e600b83d1f1
Ullasachar16 <ullasachar16@gmail.com>
2022-12-19 15:18:07 +0530

    Commit

---
+++ Collections/StringMethods.java
@@ -0,0 +1,20 @@
+package Collections;
+
+public class StringMethods {
+    public static void main(String[] args) {
+        String s="Software Developer";
+        System.out.println(s.length());
+        System.out.println(s.charAt(4));
+        System.out.println(s.toLowerCase());
+        System.out.println(s.toUpperCase());
+        System.out.println(s.startsWith("Soft"));
+        System.out.println(s.endsWith("oper"));
+        System.out.println(s.indexOf('e'));
+        System.out.println(s.contains("ware"));
+        System.out.println(s.concat("in Amazon"));
+        String a="java";
+        String b="JavA";
+        System.out.println(a.equals(b));
+        System.out.println(a.equalsIgnoreCase(b));
+    }
+}
