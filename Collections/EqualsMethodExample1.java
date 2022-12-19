cec51b9054ccb6d9b1c034538c0e4c25e7389fb5
Ullasachar16 <ullasachar16@gmail.com>
2022-12-19 14:14:07 +0530

    Commit

---
+++ Collections/EqualsMethodExample1.java
@@ -0,0 +1,18 @@
+package Collections;
+
+public class EqualsMethodExample1 {
+    int marks;
+    public EqualsMethodExample1(int marks) {
+        this.marks = marks;
+    }
+    public boolean equals(Object obj){ //Object obj=new EqualsMethod();
+        EqualsMethodExample1 e=(EqualsMethodExample1) obj;
+        return this.marks==e.marks;  //e1.marks==e2.marks
+    }
+    public static void main(String[] args) {
+        EqualsMethodExample1 e1=new EqualsMethodExample1(96);
+        EqualsMethodExample1 e2=new EqualsMethodExample1(96);
+        System.out.println(e1==e2);
+        System.out.println(e1.equals(e2));
+    }
+}
