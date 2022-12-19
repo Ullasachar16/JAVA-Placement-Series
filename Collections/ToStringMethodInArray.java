3dd6dbd97ac3e56c2813be1c67e273affa3b88e5
Ullasachar16 <ullasachar16@gmail.com>
2022-12-19 13:53:04 +0530

    Commit

---
+++ Collections/ToStringMethodInArray.java
@@ -0,0 +1,21 @@
+package Collections;
+
+public class ToStringMethodInArray {
+    String name;
+    double ratings;
+    public ToStringMethodInArray(String name, double ratings) {
+        this.name = name;
+        this.ratings = ratings;
+    }
+    public String toString(){
+        return "The hotel "+name+" has "+ratings;
+    }
+    public static void main(String[] args) {
+        ToStringMethodInArray t1=new ToStringMethodInArray("Taj",4.5);
+        ToStringMethodInArray t2=new ToStringMethodInArray("ITC",4.8);
+        ToStringMethodInArray[] t={t1,t2};
+        for(int i=0;i<t.length;i++){
+            System.out.println(t[i]);
+        }
+    }
+}
