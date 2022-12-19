dde512795a98feda41bfdec4cb992e360bd2276b
Ullasachar16 <ullasachar16@gmail.com>
2022-12-19 13:53:16 +0530

    Commit

---
+++ Collections/HashCodeMethod.java
@@ -0,0 +1,11 @@
+package Collections;
+
+public class HashCodeMethod {
+    public int hashCode(){
+        return 1;
+    }
+    public static void main(String[] args) {
+        HashCodeMethod h=new HashCodeMethod();
+        System.out.println(h.hashCode());
+    }
+}
