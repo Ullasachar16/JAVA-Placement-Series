bef61fbbeb8078a85bb23ebe9ae85e39c077a1fc
Ullasachar16 <ullasachar16@gmail.com>
2022-12-20 14:17:26 +0530

    Commit

---
+++ CustomException/AgeInvalidException.java
@@ -0,0 +1,31 @@
+package CustomException;
+
+import java.util.Scanner;
+
+public class AgeInvalidException extends Exception{
+    private String message;
+    AgeInvalidException(String message){
+        this.message=message;
+    }
+    @Override
+    public String getMessage(){
+        return message;
+    }
+}
+class Vote{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter age:");
+        int age=sc.nextInt();
+        if(age>=18){
+            System.out.println("Eligible to vote");
+        }else{
+            try {
+                throw new AgeInvalidException("Hey kiddo, wait for ur time");
+            }
+            catch (Exception e){
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+}
