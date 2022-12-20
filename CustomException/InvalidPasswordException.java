4f114a9a0bc19f9ae6814e6caf203da4750c2c51
Ullasachar16 <ullasachar16@gmail.com>
2022-12-20 14:03:32 +0530

    Commit

---
+++ CustomException/InvalidPasswordException.java
@@ -0,0 +1,31 @@
+package CustomException;
+
+import java.util.Scanner;
+
+public class InvalidPasswordException extends RuntimeException{
+
+}
+class LoginForm{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter user id:");
+        String id= sc.next();
+        System.out.println("Enter password:");
+        int password= sc.nextInt();
+        if(id.equals("admin")){
+            if(password==123){
+                System.out.println("Login Successful");
+            }else {
+                try {
+                    InvalidPasswordException obj = new InvalidPasswordException();
+                    throw obj;
+                    //throw new InvalidPasswordException();
+                }
+                catch (InvalidPasswordException e){
+                    System.out.println("Invalid Password Entered!!!");
+                }
+            }
+        }
+        sc.close();
+    }
+}
