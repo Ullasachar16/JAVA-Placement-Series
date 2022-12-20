187bb5506a6f9b331c31641936a4ca8f0ed8006b
Ullasachar16 <ullasachar16@gmail.com>
2022-12-20 13:38:55 +0530

    Commit

---
+++ ExceptionHandling/ThrowsKeywordExampleInCheckException.java
@@ -0,0 +1,31 @@
+package ExceptionHandling;
+
+import java.io.FileReader;
+
+public class ThrowsKeywordExampleInCheckException {
+    static void display()throws Exception{
+        for(int i=1;i<=5;i++){
+            System.out.println(i);
+            Thread.sleep(2000);
+        }
+    }
+    void readData()throws Exception{
+        FileReader f=new FileReader("dinga.txt");
+    }
+    public static void main(String[] args) {
+        System.out.println("Start");
+        ThrowsKeywordExampleInCheckException t=new ThrowsKeywordExampleInCheckException();
+        try {
+            display();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        try {
+            t.readData();
+        }
+        catch (Exception e){
+            System.out.println("File Not Present");
+        }
+        System.out.println("End");
+    }
+}
