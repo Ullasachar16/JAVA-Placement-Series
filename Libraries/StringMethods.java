1f6342112e2a322c99034fb00ada999fbb11932f
Ullasachar16 <ullasachar16@gmail.com>
2022-12-20 11:55:25 +0530

    Commit

--- Libraries/StringMethods.java
+++ Libraries/StringMethods.java
@@ -1,4 +1,4 @@
-package Collections;
+package Libraries;
 
 public class StringMethods {
     public static void main(String[] args) {
--- Libraries/ToStringMethodInArray.java
+++ Libraries/ToStringMethodInArray.java
@@ -1,4 +1,4 @@
-package Collections;
+package Libraries;
 
 public class ToStringMethodInArray {
     String name;
