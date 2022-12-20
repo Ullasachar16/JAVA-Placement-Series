e641bb3466cb40e138a5701fca1f78b6ef830942
Ullasachar16 <ullasachar16@gmail.com>
2022-12-20 11:54:51 +0530

    Commit

--- Libraries/HashCodeMethod.java
+++ Libraries/HashCodeMethod.java
@@ -1,4 +1,4 @@
-package Collections;
+package Libraries;
 
 public class HashCodeMethod {
     public int hashCode(){
--- Libraries/PalindromeString.java
+++ Libraries/PalindromeString.java
@@ -1,4 +1,4 @@
-package Collections;
+package Libraries;
 
 public class PalindromeString {
     public static void main(String[] args) {
