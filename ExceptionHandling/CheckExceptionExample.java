4debff901ece58cb821ba1403ab17766d1d6a92f
Ullasachar16 <ullasachar16@gmail.com>
2022-12-20 13:14:39 +0530

    Commit

---
+++ ExceptionHandling/CheckExceptionExample.java
@@ -0,0 +1,25 @@
+package ExceptionHandling;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class CheckExceptionExample {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            System.out.println(i);
+            try {
+                Thread.sleep(2000);
+            }
+            catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+        try {
+            FileReader f = new FileReader("guldu.txt");
+        }catch (FileNotFoundException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
+//FileNotFoundException -> Checked Exception(compiler known exception
+// and it force user handle it immediately)
