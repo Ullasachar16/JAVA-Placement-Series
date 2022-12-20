ab7362f823fce7931d3d2ce6217569b77d211be9
Ullasachar16 <ullasachar16@gmail.com>
2022-12-20 12:49:47 +0530

    Commit

---
+++ ExceptionHandling/ThrowableMethods.java
@@ -0,0 +1,14 @@
+package ExceptionHandling;
+
+public class ThrowableMethods {
+    public static void main(String[] args) {
+        System.out.println("Start");
+        try{
+            System.out.println(10/0);
+        }catch (Exception e){
+            System.out.println(e.getMessage());  //-> / by zero
+            //e.printStackTrace(); //-> java.lang.ArithmeticException: / by zero at ExceptionHandling.ThrowableMethods.main(ThrowableMethods.java:7)
+        }
+        System.out.println("End");
+    }
+}
