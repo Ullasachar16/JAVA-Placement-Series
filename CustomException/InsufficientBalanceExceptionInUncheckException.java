8f26e4b8c6550ea26388870f3ee84fbd514e6bde
Ullasachar16 <ullasachar16@gmail.com>
2022-12-20 14:18:04 +0530

    Commit

---
+++ CustomException/InsufficientBalanceExceptionInUncheckException.java
@@ -0,0 +1,32 @@
+package CustomException;
+
+import java.util.Scanner;
+
+public class InsufficientBalanceExceptionInUncheckException extends RuntimeException {
+    private String message;
+    InsufficientBalanceExceptionInUncheckException(String message){
+        this.message=message;
+    }
+    @Override
+    public String getMessage(){
+        return message;
+    }
+}
+class ATM1{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        double balance=10000;
+        System.out.println("Enter amount to be withdrawn:");
+        double amount= sc.nextDouble();
+        if(amount<=balance){
+            System.out.println("Withdrawn successfully");
+        }else {
+            try {
+                throw new InsufficientBalanceExceptionInUncheckException("Insufficient Funds!!!");
+            }
+            catch (Exception e){
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+}
