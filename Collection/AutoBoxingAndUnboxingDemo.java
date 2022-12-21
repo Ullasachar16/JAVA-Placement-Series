0c079bb02a2c65620856ca728a254ef596012a17
Ullasachar16 <ullasachar16@gmail.com>
2022-12-21 15:18:38 +0530

    Commit

---
+++ Collection/AutoBoxingAndUnboxingDemo.java
@@ -0,0 +1,20 @@
+package Collection;
+
+public class AutoBoxingAndUnboxingDemo {
+    public static void main(String[] args) {
+        //primitive way of representing 10
+        int a=10;
+        //non-primitive way of representing 10
+        Integer b=20;
+        //primitive way of representing 'z'
+        char c='z';
+        //non-primitive way of representing 'z'
+        Character x=new Character('z');
+        //auto boxing
+        Integer i=new Integer(a); //converting primitive datatype to non primitive
+        System.out.println(b);
+        //auto unboxing
+        char ch=x; //converting non primitive to primitive datatype
+        System.out.println(c);
+    }
+}
