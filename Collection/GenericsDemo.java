500c208104a238064ceca39c5f21488857a2c547
Ullasachar16 <ullasachar16@gmail.com>
2022-12-21 18:55:00 +0530

    Commit

--- Collection/GenericsDemo.java
+++ Collection/GenericsDemo.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class GenericsDemo {
+
     public static void main(String[] args) {
+        int a=20;
         ArrayList<String> l=new ArrayList<String>();
         l.add("20");
         l.add("sql");
@@ -17,6 +19,13 @@ public class GenericsDemo {
         ll.add(20);
         ll.add(12);
         for(Integer i:ll){
+            System.out.println(i);
+        }
+        ArrayList<Integer> al=new ArrayList();
+        al.add(a);
+        al.add(a+30);
+        al.add(a-10);
+        for(Integer i:al){
             System.out.println(i);
         }
     }
