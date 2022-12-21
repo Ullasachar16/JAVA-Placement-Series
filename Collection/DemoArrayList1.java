4aff09cea677e5fb898b1eb3380d7561bc957081
Ullasachar16 <ullasachar16@gmail.com>
2022-12-21 14:15:59 +0530

    Commit

---
+++ Collection/DemoArrayList1.java
@@ -0,0 +1,32 @@
+package Collection;
+
+import java.util.ArrayList;
+
+public class DemoArrayList1 {
+    public static void main(String[] args) {
+        ArrayList x=new ArrayList();
+        x.add(10);
+        x.add("java");
+        x.add(10);
+        x.add("sql");
+        x.add(20);
+        x.add(30);
+        x.add(34.5);
+        x.add('A');
+        x.add(true);
+        System.out.println(x);
+        for(int i=0;i<x.size();i++){
+            System.out.print(x.get(i)+",");
+        }
+        System.out.println();
+        for(int i=x.size()-1;i>=0;i--){
+            System.out.print(x.get(i)+",");
+        }
+        System.out.println();
+        ArrayList l=new ArrayList(x);
+        l.add(40);
+        l.set(8,"web");
+        System.out.println(l);
+
+    }
+}
