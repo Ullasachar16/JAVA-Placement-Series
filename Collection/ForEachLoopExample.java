85e2defe1d938c8498e9edde388f107f82945150
Ullasachar16 <ullasachar16@gmail.com>
2022-12-21 15:06:37 +0530

    Commit

---
+++ Collection/ForEachLoopExample.java
@@ -0,0 +1,19 @@
+package Collection;
+
+public class ForEachLoopExample {
+    public static void main(String[] args) {
+        int[] a={10,20,30,40};
+        for(int i=0;i<a.length;i++){
+            System.out.println(a[i]);
+        }
+        System.out.println("--------");
+        for(int i:a){
+            System.out.println(i);
+        }
+        System.out.println("----------");
+        double[] percentage={1.2,3.5,5.6};
+        for(double d:percentage){
+            System.out.println(d);
+        }
+    }
+}
