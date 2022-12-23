650547148d5e442cedfa60749ae398fc246a045a
Ullasachar16 <ullasachar16@gmail.com>
2022-12-23 12:10:18 +0530

    Commit

---
+++ Collection/PriorityQueueExample.java
@@ -0,0 +1,19 @@
+package Collection;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class PriorityQueueExample {
+    public static void main(String[] args) {
+        Queue q=new PriorityQueue();
+        q.add(10);
+        q.add(20);
+        q.add(30);
+        System.out.println(q.element());
+        System.out.println(q.peek());
+        System.out.println(q.remove());
+        System.out.println(q.remove());
+        System.out.println(q.remove());
+        System.out.println(q.poll());
+    }
+}
