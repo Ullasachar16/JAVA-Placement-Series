51d5f202cbab58b7cd4b58d605db98b249ccdb6a
Ullasachar16 <ullasachar16@gmail.com>
2022-12-24 13:10:44 +0530

    Commit

---
+++ MultiThreading/ThreadClassExample.java
@@ -0,0 +1,42 @@
+package MultiThreading;
+
+public class ThreadClassExample extends Thread {
+    @Override
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i:" + i);
+            try {
+                Thread.sleep(1500);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+}
+class Student2 extends Thread{
+    @Override
+    public void run(){
+        for(int j=6;j<=10;j++){
+            System.out.println("j:"+j);
+            try{
+                Thread.sleep(1500);
+            }catch (Exception e){
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+}
+class Solution{
+    public static void main(String[] args) {
+        Student2 s=new Student2();
+        //s.setName("2nd Thread");
+        System.out.println(s.getName());
+        s.start();
+        ThreadClassExample tce=new ThreadClassExample();
+        tce.setName("1st thread");
+        System.out.println(tce.getName());
+        tce.start();
+
+    }
+}
