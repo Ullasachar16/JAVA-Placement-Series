5be36ae83c01b1cf6f527745a1d035520cb9d0ea
Ullasachar16 <ullasachar16@gmail.com>
2022-12-24 13:35:35 +0530

    Commit

---
+++ MultiThreading/RunnableInterfaceExample.java
@@ -0,0 +1,26 @@
+package MultiThreading;
+
+public class RunnableInterfaceExample implements Runnable{
+    @Override
+    public void run(){
+        System.out.println("Purchasing Laptop");
+    }
+}
+class Flipkart implements Runnable{
+    @Override
+    public void run(){
+        System.out.println("Purchasing Mobile");
+    }
+}
+class Solution1{
+    public static void main(String[] args) {
+        RunnableInterfaceExample r=new RunnableInterfaceExample();
+        Thread t1=new Thread(r,"Amazon Thread");
+        System.out.println(t1.getName());
+        t1.start();
+        Flipkart f=new Flipkart();
+        Thread t2=new Thread(f,"Flipkart Thread");
+        System.out.println(t2.getName());
+        t2.start();
+    }
+}
