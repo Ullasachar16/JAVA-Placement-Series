bf6be87983b11aecca6b99b276fe4cb0c1cabf1e
Ullasachar16 <ullasachar16@gmail.com>
2022-12-25 14:45:54 +0530

    Commit

---
+++ DataStructures/LinkedListsExample.java
@@ -0,0 +1,22 @@
+package DataStructures;
+
+import java.util.LinkedList;
+
+public class LinkedListsExample {
+    public static void main(String[] args) {
+        LinkedList<Integer>l=new LinkedList<>();
+        l.add(5);
+        l.add(7);
+        l.add(3);
+        l.addFirst(1);
+        l.addLast(8);
+        l.add(2);
+        l.add(3);
+        int index=0;
+        System.out.println(l);
+        for(int i=0;i<l.size();i++){
+            index=l.indexOf(7);
+        }
+        System.out.println("index:"+index);
+    }
+}
