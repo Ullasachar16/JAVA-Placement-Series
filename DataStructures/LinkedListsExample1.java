b0acca6b6ffe70d5eed3ff263c8078112bfe864d
Ullasachar16 <ullasachar16@gmail.com>
2022-12-25 15:00:25 +0530

    Commit

---
+++ DataStructures/LinkedListsExample1.java
@@ -0,0 +1,24 @@
+package DataStructures;
+
+import java.util.LinkedList;
+
+public class LinkedListsExample1 {
+    public static void main(String[] args) {
+        LinkedList<Integer> l = new LinkedList<>();
+        l.add(10);
+        l.add(24);
+        l.add(45);
+        l.add(34);
+        l.add(20);
+        l.add(15);
+        l.add(26);
+        System.out.println("before removing");
+        System.out.print(l+" ");
+        System.out.println();
+        System.out.println("after removing");
+        for(int i=0;i<l.size();i++){
+
+        }
+
+    }
+}
