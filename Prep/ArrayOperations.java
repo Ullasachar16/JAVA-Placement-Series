696dc28f2a2e77b9a70bc1644895365954e35fd1
Ullasachar16 <ullasachar16@gmail.com>
2022-12-26 13:00:30 +0530

    Commit

---
+++ Prep/ArrayOperations.java
@@ -0,0 +1,23 @@
+package Prep;
+
+public class ArrayOperations {
+    public static void main(String[] args) {
+        double[] myList={1.9,2.9,3.4,3.5};
+        double total=0;
+        System.out.println("array elements are:");
+        for(int i=0;i<myList.length;i++){
+            System.out.println(myList[i]+" ");
+        }
+        for(int i=0;i<myList.length;i++){
+           total+=myList[i];
+        }
+        System.out.println("Total:"+total);
+        double max=myList[0];
+        for(int i=0;i<myList.length;i++){
+            if(myList[i]>max){
+                max=myList[i];
+            }
+        }
+        System.out.println("max:"+max);
+    }
+}
