e67a6ff47b78a9fe073ff262a740a7d1488228d9
Ullasachar16 <ullasachar16@gmail.com>
2022-12-26 13:35:14 +0530

    Commit

---
+++ DataStructures/ReverseLL.java
@@ -0,0 +1,120 @@
+package DataStructures;
+
+class ReverseLL {
+    //Using Iterative Method
+
+    Node head;
+    private int size;
+
+    ReverseLL () {
+        size = 0;
+    }
+
+    public class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+            size++;
+        }
+    }
+
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+
+        if(head == null) {
+            head = newNode;
+            return;
+        }
+
+        Node lastNode = head;
+        while(lastNode.next != null) {
+            lastNode = lastNode.next;
+        }
+
+        lastNode.next = newNode;
+    }
+
+    public void printList() {
+        Node currNode = head;
+
+        while(currNode != null) {
+            System.out.print(currNode.data+" -> ");
+            currNode = currNode.next;
+        }
+
+        System.out.println("null");
+    }
+
+    public void removeFirst() {
+        if(head == null) {
+            System.out.println("Empty List, nothing to delete");
+            return;
+        }
+
+        head = this.head.next;
+        size--;
+    }
+
+    public void removeLast() {
+        if(head == null) {
+            System.out.println("Empty List, nothing to delete");
+            return;
+        }
+
+        size--;
+        if(head.next == null) {
+            head = null;
+            return;
+        }
+
+        Node currNode = head;
+        Node lastNode = head.next;
+        while(lastNode.next != null) {
+            currNode = currNode.next;
+            lastNode = lastNode.next;
+        }
+
+        currNode.next = null;
+    }
+
+    public int getSize() {
+        return size;
+    }
+    public void reverseIterate(){
+        if(head==null || head.next==null){
+            return;
+        }
+        Node prevNode=head;
+        Node currNode=head.next;
+        while(currNode!=null){
+            Node nextNode=currNode.next;
+            currNode.next=prevNode;
+            //update
+            prevNode=currNode;
+            currNode=nextNode;
+        }
+        head.next=null;
+        head=prevNode;
+    }
+
+    public static void main(String args[]) {
+        ReverseLL list = new ReverseLL();
+        list.addLast(1);
+        list.addLast(2);
+        list.addLast(3);
+        list.addLast(4);
+        list.printList();
+        list.reverseIterate();
+        list.printList();
+    }
+}
+
