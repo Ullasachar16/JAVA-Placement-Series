8f72b89668152bea6f42bbda06cf21e77cb77dc5
Ullasachar16 <ullasachar16@gmail.com>
2022-12-26 13:53:42 +0530

    Commit

---
+++ Prep/PrintingAllSubstrings.java
@@ -0,0 +1,21 @@
+package Prep;
+
+import java.util.Scanner;
+
+public class PrintingAllSubstrings {
+    public static void main(String[] args) {
+        String str,sub;
+        int i,c,length;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a string to print its all substrings");
+        str=sc.nextLine();
+        length=str.length();
+        System.out.println("Substrings of "+str+" are:");
+        for(c=0;c<length;c++){
+            for(i=1;i<=length-c;i++){
+                sub=str.substring(c,c+i);
+                System.out.println(sub);
+            }
+        }
+    }
+}
