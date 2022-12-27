70b9a83e6d3baa320adc313af842707aa4f158bd
Ullasachar16 <ullasachar16@gmail.com>
2022-12-27 13:46:28 +0530

    Commit

---
+++ Prep/CountNoOfVowelsInAString.java
@@ -0,0 +1,24 @@
+package Prep;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class CountNoOfVowelsInAString {
+    public static void main(String[] args) {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter String:");
+        String str=null;
+        try {
+            str = br.readLine();
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        int noOfVowels=0;
+        for(int i=0;i<str.length();i++){
+            if((str.toLowerCase().charAt(i)=='a')||(str.toLowerCase().charAt(i)=='e')||(str.toLowerCase().charAt(i)=='i')||(str.toLowerCase().charAt(i)=='o')||(str.toLowerCase().charAt(i)=='u')){
+                noOfVowels++;
+            }
+        }
+        System.out.println("Number of vowels in "+str+" :"+noOfVowels);
+    }
+}
