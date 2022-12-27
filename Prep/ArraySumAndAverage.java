4a7f7f8e392f3629b60cac570d8415726f4aca58
Ullasachar16 <ullasachar16@gmail.com>
2022-12-27 13:35:36 +0530

    Commit

---
+++ Prep/ArraySumAndAverage.java
@@ -0,0 +1,13 @@
+package Prep;
+
+public class ArraySumAndAverage {
+    public static void main(String[] args) {
+        int a[]={1,4,7,9,13,15,17};
+        int sum=0;
+        for(int i=0;i< a.length;i++){
+            sum+=a[i];
+        }
+        System.out.println("sum:"+sum);
+        System.out.println("average:"+(sum/a.length));
+    }
+}
