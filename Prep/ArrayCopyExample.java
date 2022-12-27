55296355b1f5617834e67be60898039ba3fcd681
Ullasachar16 <ullasachar16@gmail.com>
2022-12-27 13:11:24 +0530

    Commit

---
+++ Prep/ArrayCopyExample.java
@@ -0,0 +1,10 @@
+package Prep;
+
+public class ArrayCopyExample {
+    public static void main(String[] args) {
+        char[] copyFrom={'d','e','c','c','a','n','c','h','a','r','g','e','r','s'};
+        char[] copyTo=new char[15];
+        System.arraycopy(copyFrom,0,copyTo,0,14);
+        System.out.println(new String(copyTo));
+    }
+}
