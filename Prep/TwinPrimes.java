38e7173eb87cd345ddd79148dbdfc29ff5202b1d
Ullasachar16 <ullasachar16@gmail.com>
2022-12-28 13:18:02 +0530

    Commit

---
+++ Prep/TwinPrimes.java
@@ -0,0 +1,25 @@
+package Prep;
+
+public class TwinPrimes {
+    public static void main(String[] args) {
+        String primeNo="";
+        int j=0;
+        int lastPrime=1;
+        System.out.println("Twin Primes are:");
+        for(int i=1;i<100;i++){
+            for(j=2;j<i;j++){
+                if(i%j==0){
+                    break;
+                }
+            }
+            if(i==j){
+                primeNo+=i+" ";
+                if((i-lastPrime)==2){
+                    System.out.println("("+(i-2)+"."+i+")");
+                }
+                lastPrime=i;
+            }
+        }
+        System.out.println("Prime Numbers are:"+primeNo);
+    }
+}
