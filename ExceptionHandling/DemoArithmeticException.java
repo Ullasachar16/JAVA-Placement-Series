cd4f1a5243d529fae89c9023bf918b00560275f1
Ullasachar16 <ullasachar16@gmail.com>
2023-01-04 14:02:03 +0530

    Commit

--- ExceptionHandling/DemoArithmeticException.java
+++ ExceptionHandling/DemoArithmeticException.java
@@ -3,6 +3,7 @@ package ExceptionHandling;
 import java.util.Scanner;
 
 public class DemoArithmeticException {
+    //Implementing arithmetic exception
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter a:");
