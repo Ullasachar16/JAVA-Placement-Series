2073845fec96e323ead6354eca5b38cd531e9f8a
Ullasachar16 <ullasachar16@gmail.com>
2023-01-09 14:49:13 +0530

    Commit

--- Backtracking/NQueensProblem.java
+++ Backtracking/NQueensProblem.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class NQueensProblem {
+    //n Queens Problem using backtracking
     public static boolean isSafe(int row,int col,char[][]board){
         //horizontal
         for(int j=0;j<board.length;j++){
