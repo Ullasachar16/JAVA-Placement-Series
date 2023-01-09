4d791b335858b732aa69906233a4efd9034dec7a
Ullasachar16 <ullasachar16@gmail.com>
2023-01-09 14:53:01 +0530

    Commit

--- CustomException/InsufficientBalanceExceptionInUncheckException.java
+++ CustomException/InsufficientBalanceExceptionInUncheckException.java
@@ -13,6 +13,7 @@ public class InsufficientBalanceExceptionInUncheckException extends RuntimeExcep
     }
 }
 class ATM1{
+    //program to implement insufficientbalance exception using uncheck exception
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         double balance=10000;
