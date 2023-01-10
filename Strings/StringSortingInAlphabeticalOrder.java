8b7626e3eac8a8100ad5a453954a08dab32d8886
Ullasachar16 <ullasachar16@gmail.com>
2023-01-10 14:13:20 +0530

    Commit

--- Strings/StringSortingInAlphabeticalOrder.java
+++ Strings/StringSortingInAlphabeticalOrder.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class StringSortingInAlphabeticalOrder {
+    //program to sort string in alphabetical order
     public static void main(String[] args) throws IOException {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 
