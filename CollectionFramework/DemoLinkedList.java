44dfec395a69ddbeca2b502ebdf3c3c566c2aaec
Ullasachar16 <ullasachar16@gmail.com>
2023-01-11 14:15:23 +0530

    Commit

--- CollectionFramework/DemoLinkedList.java
+++ CollectionFramework/DemoLinkedList.java
@@ -3,6 +3,7 @@ package CollectionFramework;
 import java.util.LinkedList;
 
 public class DemoLinkedList{
+    //demo program for linkedlist
     public static void main(String[] args) {
         LinkedList l=new LinkedList();
         l.add(10);
