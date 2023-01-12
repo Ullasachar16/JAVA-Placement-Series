f9754983f9d1a73f3941293e1991b9a32cef0777
Ullasachar16 <ullasachar16@gmail.com>
2023-01-12 13:53:21 +0530

    Commit

--- CollectionFramework/LinkedHashSetExample.java
+++ CollectionFramework/LinkedHashSetExample.java
@@ -3,6 +3,7 @@ package CollectionFramework;
 import java.util.LinkedHashSet;
 
 public class LinkedHashSetExample {
+    //example program of linked hash set
     public static void main(String[] args) {
         LinkedHashSet lhs=new LinkedHashSet();
         lhs.add("java");
