7e2205afad5b81c944c528f21a21e2d52de8ee9f
Ullasachar16 <ullasachar16@gmail.com>
2023-01-12 13:52:55 +0530

    Commit

--- CollectionFramework/IteratorInterfaceExample.java
+++ CollectionFramework/IteratorInterfaceExample.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 public class IteratorInterfaceExample {
+    //program to show iterator interface
     public static void main(String[] args) {
         ArrayList l=new ArrayList<>();
         l.add(10);
