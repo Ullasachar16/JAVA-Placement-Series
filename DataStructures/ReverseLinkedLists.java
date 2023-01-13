93df9f4d6266996e36ae1a486f382e19d54647cd
Ullasachar16 <ullasachar16@gmail.com>
2023-01-13 14:30:22 +0530

    Commit

--- DataStructures/ReverseLinkedLists.java
+++ DataStructures/ReverseLinkedLists.java
@@ -4,6 +4,7 @@ import java.util.Collections;
 import java.util.LinkedList;
 
 public class ReverseLinkedLists {
+    //reverse linked list example
     public static void main(String[] args) {
         LinkedList l=new LinkedList();
         l.add(11);
