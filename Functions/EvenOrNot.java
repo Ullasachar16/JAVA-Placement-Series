eb75176ceba71caabbbfdcce484517d6660636ce
Ullasachar16 <ullasachar16@gmail.com>
2023-01-14 15:00:19 +0530

    Commit

--- Functions/EvenOrNot.java
+++ Functions/EvenOrNot.java
@@ -3,6 +3,7 @@ package Functions;
 import java.util.Scanner;
 
 public class EvenOrNot {
+    //program to check even or not
     public static void isEven(int n){
         if(n%2==0){
             System.out.println("Even");
