3c6b290a3880d3701c00c24e4c2bf029d129cd80
Ullasachar16 <ullasachar16@gmail.com>
2023-01-15 13:24:58 +0530

    Commit

--- Libraries/PalindromeString.java
+++ Libraries/PalindromeString.java
@@ -1,6 +1,7 @@
 package Libraries;
 
 public class PalindromeString {
+    //program to check for string palindrome
     public static void main(String[] args) {
         String s="madan";
         String s1="";
