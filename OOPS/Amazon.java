ea614bcbcd7db2a027db06337646f019ed22c30e
Ullasachar16 <ullasachar16@gmail.com>
2023-01-15 13:24:28 +0530

    Commit

--- OOPS/Amazon.java
+++ OOPS/Amazon.java
@@ -1,17 +1,21 @@
 package OOPS;
 
 public class Amazon {
+    //program to implement interface operation
     void work(){
         System.out.println("Working at Amazon");
     }
 }
 interface Jspiders{
+    //extending interface
     void developApp();
 }
 interface Qspiders{
+    //extending interface
     void testApp();
 }
 class Employe extends Amazon implements Jspiders,Qspiders{
+    //class extending class and implementing interfaces
     @Override
     public void developApp(){
         System.out.println("Developing App");
