e4364d9044fac6718201a4acdacb997b6e428148
Ullasachar16 <ullasachar16@gmail.com>
2023-01-19 15:46:23 +0530

    Commit

--- OOPS/SingleTonDesignPattern2.java
+++ OOPS/SingleTonDesignPattern2.java
@@ -1,6 +1,7 @@
 package OOPS;
 
 public class SingleTonDesignPattern2 {
+    //another example program for single ton design pattern
     public static void main(String[] args) {
         AadhaarCard ac=AadhaarCard.getInstance();
         System.out.println("Name:"+ac.name);
