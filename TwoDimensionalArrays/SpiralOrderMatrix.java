ba5ed5e62ecaedbf60808bc4e79b0c0b1f75b4d9
Ullasachar16 <ullasachar16@gmail.com>
2023-01-22 12:37:44 +0530

    Commit

--- TwoDimensionalArrays/SpiralOrderMatrix.java
+++ TwoDimensionalArrays/SpiralOrderMatrix.java
@@ -3,6 +3,7 @@ package TwoDimensionalArrays;
 import java.util.Scanner;
 
 public class SpiralOrderMatrix {
+    //program to implement spiral order matrix
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter row elements:");
