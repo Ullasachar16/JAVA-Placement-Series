298f8cdcfbcaa6212f1b3603122a13aad9248580
Ullasachar16 <ullasachar16@gmail.com>
2023-01-23 11:04:52 +0530

    Commit

--- BasicFundamentals/Practice.java
+++ BasicFundamentals/Practice.java
@@ -1,6 +1,7 @@
 package BasicFundamentals;
 
 public class Practice {
+    //practice program of pattern
     public static void main(String[] args) {
         int n=5;
         for(int i=1;i<=n;i++){
