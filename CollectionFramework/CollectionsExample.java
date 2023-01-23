f9fdefc0665d3160a11aa42fd921d478ef284a07
Ullasachar16 <ullasachar16@gmail.com>
2023-01-23 11:07:46 +0530

    Commit

--- CollectionFramework/CollectionsExample.java
+++ CollectionFramework/CollectionsExample.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class CollectionsExample {
-    //program to implement collections class in collection framework
+    //program to implement collections class in collection framework example 1
     public static void main(String[] args) {
         ArrayList l=new ArrayList();
         l.add(10);
