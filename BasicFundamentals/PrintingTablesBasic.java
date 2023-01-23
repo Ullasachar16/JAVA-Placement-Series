fb6361d137bf5ab27894ec988711139bc003c0ba
Ullasachar16 <ullasachar16@gmail.com>
2023-01-23 11:05:39 +0530

    Commit

--- BasicFundamentals/PrintingTablesBasic.java
+++ BasicFundamentals/PrintingTablesBasic.java
@@ -3,6 +3,7 @@ package BasicFundamentals;
 import java.util.Scanner;
 
 public class PrintingTablesBasic {
+    //program to print tables according to users input
     public static void main(String[] args) {
         //print tables according to user's input
         Scanner sc=new Scanner(System.in);
