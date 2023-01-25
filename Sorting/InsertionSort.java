7d39ab85b3a33dd9648785cde9da88d3793166ef
Ullasachar16 <ullasachar16@gmail.com>
2023-01-25 11:06:06 +0530

    Commit

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -1,6 +1,7 @@
 package Sorting;
 
 public class InsertionSort {
+    //program to implement insertion sort
     public static void printArray(int arr[]){
         for(int i=0;i< arr.length;i++){
             System.out.print(arr[i]+" ");
