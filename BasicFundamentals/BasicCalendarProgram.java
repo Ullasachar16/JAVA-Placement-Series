255b9218c37200b05632a8132a20d56c9933ae86
Ullasachar16 <ullasachar16@gmail.com>
2023-02-09 12:36:55 +0530

    Commit

--- BasicFundamentals/BasicCalendarProgram.java
+++ BasicFundamentals/BasicCalendarProgram.java
@@ -3,7 +3,7 @@ package BasicFundamentals;
 import java.util.Scanner;
 
 public class BasicCalendarProgram {
-    //program to implement basic calendar
+    //Program to implement basic calendar using switch statement
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter number of month:");
