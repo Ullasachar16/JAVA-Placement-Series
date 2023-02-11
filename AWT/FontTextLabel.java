f053c00ef2904fab4d292dafa41913025f1c33af
Ullasachar16 <ullasachar16@gmail.com>
2023-02-11 12:10:31 +0530

    Commit

---
+++ AWT/FontTextLabel.java
@@ -0,0 +1,28 @@
+package AWT;
+import java.awt.*;
+public class FontTextLabel extends Frame{
+    //Declare component label
+    Label lb1,lb2;
+    public FontTextLabel(){
+        setLayout(new FlowLayout());
+        //constructor label
+        lb1=new Label("Text Font Label");
+        lb2=new Label("Normal Text Label");
+
+        // "super" Frame adds Label
+        add(lb1);
+        add(lb2);
+
+        Font myFont=new Font("Serif",Font.BOLD,12);
+        lb1.setFont(myFont);
+
+        setTitle("AWT Elements");
+        setSize(200,200);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        //Allocating an instance
+        FontTextLabel ftl=new FontTextLabel();
+    }
+}
