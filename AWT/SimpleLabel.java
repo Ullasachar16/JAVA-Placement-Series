26faf1b208760018526807b66521bfe4c13d0202
Ullasachar16 <ullasachar16@gmail.com>
2023-02-11 12:15:30 +0530

    Commit

---
+++ AWT/SimpleLabel.java
@@ -0,0 +1,25 @@
+package AWT;
+import java.awt.*;
+public class SimpleLabel extends Frame {
+    //Program to implement AWT components
+    //Declare component label
+    Label lblCount;
+    public SimpleLabel(){
+        setLayout(new FlowLayout());
+
+        //constructor label
+        lblCount = new Label("Simple Label Example");
+
+        //"super" Frame adds label
+        add(lblCount);
+
+        setTitle("AWT Elements");
+        setSize(200,200);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        //Allocating an instance
+        SimpleLabel sl=new SimpleLabel();
+    }
+}
