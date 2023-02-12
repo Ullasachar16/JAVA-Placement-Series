18edd7f4642066917bce20cfee9e6868abf5a9c8
Ullasachar16 <ullasachar16@gmail.com>
2023-02-12 12:44:58 +0530

    Commit

---
+++ Miscellaneous/RemoveZero.java
@@ -0,0 +1,25 @@
+package Miscellaneous;
+
+import java.util.Scanner;
+
+public class RemoveZero {
+    public static void main(String[] args) {
+        int a,x,b;
+        char z;
+        String n,k="",str;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        str = sc.next();
+
+        x = str.length();
+
+        for(int i=0;i<x;i++){
+            z = str.charAt(i);
+            if(z=='0'){
+
+            }else
+                k+=z;
+        }
+        System.out.println("Numbers without zero:"+k);
+    }
+}
