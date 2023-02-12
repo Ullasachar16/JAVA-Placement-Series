d07c752a5c0bc5fa6093cb73d02ad47cdf663c26
Ullasachar16 <ullasachar16@gmail.com>
2023-02-12 12:06:52 +0530

    Commit

---
+++ GUI/ToolTipExample.java
@@ -0,0 +1,37 @@
+package GUI;
+
+import javax.swing.*;
+
+public class ToolTipExample extends JFrame {
+    public ToolTipExample(){
+        initUI();
+    }
+    private void initUI(){
+        JPanel panel = new JPanel();
+        getContentPane().add(panel);
+
+        panel.setLayout(null);
+        panel.setToolTipText("This is panel");
+
+        JButton btn = new JButton("Button");
+        btn.setBounds(100,60,100,30);
+        btn.setToolTipText("This is a button");
+
+        panel.add(btn);
+
+        setTitle("Tooltip");
+        setSize(300,300);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                ToolTipExample ex = new ToolTipExample();
+                ex.setVisible(true);
+            }
+        });
+    }
+}
