7ad2ea2248106090895d5bce7f15214948b860e1
Ullasachar16 <ullasachar16@gmail.com>
2023-02-15 12:06:13 +0530

    Commit

--- GUI/ToolTipExample.java
+++ GUI/ToolTipExample.java
@@ -3,6 +3,7 @@ package GUI;
 import javax.swing.*;
 
 public class ToolTipExample extends JFrame {
+    //Program to implement ToolTip Using JFrame and GUI
     public ToolTipExample(){
         initUI();
     }
