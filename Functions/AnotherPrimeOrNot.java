5a0e0a731284e9d0548be118089795a6b32d9ede
Ullasachar16 <ullasachar16@gmail.com>
2023-02-19 13:07:46 +0530

    Commit

--- Functions/AnotherPrimeOrNot.java
+++ Functions/AnotherPrimeOrNot.java
@@ -3,7 +3,7 @@ package Functions;
 import java.util.Scanner;
 
 public class AnotherPrimeOrNot {
-    //program to check prime or not
+    //Program to check the no is prime or not
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter a number");
