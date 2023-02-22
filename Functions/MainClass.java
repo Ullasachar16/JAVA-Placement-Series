b2e0f90185cd44f6743d2df7e123d5435f046b43
Ullasachar16 <ullasachar16@gmail.com>
2023-02-22 13:47:33 +0530

    Commit

--- Functions/MainClass.java
+++ Functions/MainClass.java
@@ -1,9 +1,9 @@
 package Functions;
 import OOPS.Car4;
 public class MainClass extends Car4 {
-    //program to access class across diff package example
+    //Program to access class across diff package example
     public static void main(String[] args) {
-        //accessing public and protected in diff package
+        //Accessing public and protected in diff package
         Car4 c=new Car4();
         MainClass mc=new MainClass();
         System.out.println("car cost:"+c.cost);
