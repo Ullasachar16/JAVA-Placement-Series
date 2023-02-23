ddaef7bb48147d37fd674f5f321251bf29fb8084
Ullasachar16 <ullasachar16@gmail.com>
2023-02-23 12:25:09 +0530

    Commit

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -1,7 +1,7 @@
 package Sorting;
 
 public class BubbleSort {
-    //program to implement bubble sort
+    //Program to implement Bubble Sort Technique
     public static void printArray(int arr[]){
         for(int i=0;i< arr.length;i++){
             System.out.print(arr[i]+" ");
