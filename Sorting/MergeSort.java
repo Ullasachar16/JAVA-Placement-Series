e1c73e0163fd27a6afb4e8d95db64381d248bea4
Ullasachar16 <ullasachar16@gmail.com>
2023-02-23 12:26:16 +0530

    Commit

--- Sorting/MergeSort.java
+++ Sorting/MergeSort.java
@@ -1,7 +1,7 @@
 package Sorting;
 
 public class MergeSort {
-    //program to implement merge sort
+    //Program to implement Merge Sort Technique
     public static void conquer(int arr[],int si,int mid,int ei){
         //n log n
         int merged[]=new int[ei-si+1];
