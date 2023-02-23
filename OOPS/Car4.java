d5663e15f24a84de5b84e57a3a64ccc8d03a84d9
Ullasachar16 <ullasachar16@gmail.com>
2023-02-23 12:25:18 +0530

    Commit

--- OOPS/Car4.java
+++ OOPS/Car4.java
@@ -1,7 +1,7 @@
 package OOPS;
 
 public class Car4 {
-    //accessing public and protected in diff package
+    //Accessing public and protected in diff package
     public int cost=500000;
     protected String name="Nissan GTR";
 }
