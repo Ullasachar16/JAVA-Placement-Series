c567436fdc5ab1854d53323632dd1dea6717520d
Ullasachar16 <ullasachar16@gmail.com>
2023-02-23 12:26:36 +0530

    Commit

--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -1,7 +1,7 @@
 package Sorting;
 
 public class SelectionSort {
-    //program to implement selection sort
+    //Program to implement Selection Sort Technique
     public static void printArray(int arr[]){
         for(int i=0;i< arr.length;i++){
             System.out.print(arr[i]+" ");
