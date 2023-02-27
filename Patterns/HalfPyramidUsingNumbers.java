8d6fe13b3cbe5ebee79de3c7514af94884d2b8b8
Ullasachar16 <ullasachar16@gmail.com>
2023-02-27 11:35:06 +0530

    Commit

--- Patterns/HalfPyramidUsingNumbers.java
+++ Patterns/HalfPyramidUsingNumbers.java
@@ -1,7 +1,7 @@
 package Patterns;
 
 public class HalfPyramidUsingNumbers {
-    //program to print half pyramid pattern using numbers
+    //Program to print Half Pyramid pattern using numbers
     public static void main(String[] args) {
         /* print half pyramid pattern using numbers
                     1
