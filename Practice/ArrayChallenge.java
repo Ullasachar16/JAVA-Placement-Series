e139a21f9b259d868f6b11f6a5f05bf9cc8745e6
Ullasachar16 <ullasachar16@gmail.com>
2023-03-06 11:49:28 +0530

    Commit

--- Practice/ArrayChallenge.java
+++ Practice/ArrayChallenge.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class ArrayChallenge {
-    //program to implement array function
+    //Program to Implement Array function
     private static Scanner sc=new Scanner(System.in);
     public static void main(String[] args) {
         int[] myIntegers=getIntegers(5);
