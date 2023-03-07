e5d37ca9c0058a3a4da8baa79b5621b88a841923
Ullasachar16 <ullasachar16@gmail.com>
2023-03-07 11:32:12 +0530

    Commit

--- Practice/BasicGmailAuthentication.java
+++ Practice/BasicGmailAuthentication.java
@@ -3,7 +3,7 @@ package Practice;
 import java.util.Scanner;
 
 public class BasicGmailAuthentication {
-    //program to implement basic gmail authentication
+    //Program to implement Basic Gmail Authentication
     public static void main(String[] args) {
         String username="john";
         String password="johndoe";
