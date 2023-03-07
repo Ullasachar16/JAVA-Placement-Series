c4a58ea1b7cf25b2ac657f4f5fbb500447d2534c
Ullasachar16 <ullasachar16@gmail.com>
2023-03-07 11:32:59 +0530

    Commit

--- Practice/BasicsUnaryOperator.java
+++ Practice/BasicsUnaryOperator.java
@@ -8,7 +8,7 @@ public class BasicsUnaryOperator {
         int y=x++ + x++;
         System.out.println("x:"+x+" y:"+y);
 
-        //pre increment
+        //Pre increment
         int a=1;
         int b=++a + ++a;
         System.out.println("a:"+a+" b:"+b);
