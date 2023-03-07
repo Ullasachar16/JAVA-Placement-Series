dc3b401aa3740187eb1dc1cd4df41ca81f49a818
Ullasachar16 <ullasachar16@gmail.com>
2023-03-07 11:33:32 +0530

    Commit

--- Practice/BasicTernaryOperator.java
+++ Practice/BasicTernaryOperator.java
@@ -1,7 +1,7 @@
 package Practice;
 
 public class BasicTernaryOperator {
-    //program to show basic ternary operator
+    //Program to show Basic Ternary Operator
     public static void main(String[] args) {
         int a=80;
         int b=40;
