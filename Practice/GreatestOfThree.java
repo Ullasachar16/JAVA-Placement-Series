f00397e54ea36fbf3389844035bda2ec73356846
Ullasachar16 <ullasachar16@gmail.com>
2023-03-07 11:34:49 +0530

    Commit

--- Practice/GreatestOfThree.java
+++ Practice/GreatestOfThree.java
@@ -1,7 +1,7 @@
 package Practice;
 
 public class GreatestOfThree {
-    //Program to find Greatest of three using ternary operator
+    //Program to find Greatest of Three using Ternary Operator
     public static void main(String[] args) {
         int a=40,b=78,c=19;
         int res=c > (a>b ? a:b) ? c:((a>b) ? a:b);
