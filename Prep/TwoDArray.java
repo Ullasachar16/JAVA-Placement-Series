b34958bf8f6558bd8b70005c4dbe178150c9e2d8
Ullasachar16 <ullasachar16@gmail.com>
2023-03-08 16:00:19 +0530

    Commit

--- Prep/TwoDArray.java
+++ Prep/TwoDArray.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class TwoDArray {
+    //Program to implement 2D Array concepts
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter # of rows:");
