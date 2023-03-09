4ba1bfb7a8cf1ca1adb9496ecc540b4f2afe1cd3
Ullasachar16 <ullasachar16@gmail.com>
2023-03-09 13:02:52 +0530

    Commit

--- Recursion/FindingFirstAndLastOccurrenceOfElementInAString.java
+++ Recursion/FindingFirstAndLastOccurrenceOfElementInAString.java
@@ -1,7 +1,7 @@
 package Recursion;
 
 public class FindingFirstAndLastOccurrenceOfElementInAString {
-    //program to find first and last occurence of a element in a string
+    //Program to find first and last occurence of a element in a string
     public static int first=-1;
     public static int last=-1;
 
