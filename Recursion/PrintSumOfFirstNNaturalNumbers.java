12c1f21b28718a5bf3e0b94b8ede160363a4be2d
Ullasachar16 <ullasachar16@gmail.com>
2023-03-11 11:29:40 +0530

    Commit

--- Recursion/PrintSumOfFirstNNaturalNumbers.java
+++ Recursion/PrintSumOfFirstNNaturalNumbers.java
@@ -1,7 +1,7 @@
 package Recursion;
 
 public class PrintSumOfFirstNNaturalNumbers {
-    //Program to print sum of first n natural numbers
+    //Program to Print sum of first n natural numbers
     public static void printSum(int i,int n,int sum){
         if(i==n){
             sum+=i;
