dcd057afb4a9c597a66f7464635332e39a1cf8e0
Ullasachar16 <ullasachar16@gmail.com>
2023-03-12 12:46:53 +0530

    Commit

--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -11,7 +11,7 @@ public class SelectionSort {
     public static void main(String[] args) {
         int arr[]={7,8,3,1,2};
 
-        //time complexity = O(n^2)
+        //Time complexity = O(n^2)
          //selection sort
         for(int i=0;i<arr.length-1;i++){
             int smallest=i;
