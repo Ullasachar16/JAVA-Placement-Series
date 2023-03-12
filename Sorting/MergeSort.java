924a6e08a030236f93eb4a40f8c78356a0ad658f
Ullasachar16 <ullasachar16@gmail.com>
2023-03-12 12:45:08 +0530

    Commit

--- Sorting/MergeSort.java
+++ Sorting/MergeSort.java
@@ -40,7 +40,7 @@ public class MergeSort {
         int arr[]={6,3,9,5,2,8};
         int n= arr.length;
         divide(arr,0,n-1);
-        //print
+        //Print
         for(int i=0;i<n;i++){
             System.out.print(arr[i]+" ");
         }   System.out.println();
