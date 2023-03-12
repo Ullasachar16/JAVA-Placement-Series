626bb7069afea8e9a0a605afc654095cff8f78f9
Ullasachar16 <ullasachar16@gmail.com>
2023-03-12 12:43:34 +0530

    Commit

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -10,7 +10,7 @@ public class BubbleSort {
     }
     public static void main(String[] args) {
         int arr[]={7,8,3,1,2};
-        //time complexity =O(n^2)
+        //Time complexity =O(n^2)
          //bubble sort
         for(int i=0;i<arr.length-1;i++){ //n-1
             for(int j=0;j< arr.length-i-1;j++){
