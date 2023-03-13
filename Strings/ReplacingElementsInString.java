2725b843878870ac216c96cb36b634e2fd9f6285
Ullasachar16 <ullasachar16@gmail.com>
2023-03-13 13:48:29 +0530

    Commit

--- Strings/ReplacingElementsInString.java
+++ Strings/ReplacingElementsInString.java
@@ -3,7 +3,7 @@ package Strings;
 import java.util.Scanner;
 
 public class ReplacingElementsInString {
-    //Program to replace specific elements in a string inputted by the user
+    //Program to Replace specific elements in a string inputted by the user
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter String:");
