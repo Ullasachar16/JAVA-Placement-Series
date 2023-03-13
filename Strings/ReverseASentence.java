3520b5e6ee66ff6966e40d5bf8a5e91c7e9a0e91
Ullasachar16 <ullasachar16@gmail.com>
2023-03-13 13:48:53 +0530

    Commit

--- Strings/ReverseASentence.java
+++ Strings/ReverseASentence.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class ReverseASentence {
-    //Program to reverse a sentence using string operations
+    //Program to Reverse a sentence using string operations
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Sentence:");
