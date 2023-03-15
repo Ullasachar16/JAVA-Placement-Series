0e20f5ad7bc9d5afd83328cd7adc8b7ca13a9f05
Ullasachar16 <ullasachar16@gmail.com>
2023-03-15 11:54:38 +0530

    Commit

--- Threads/RunnableInterfaceExample.java
+++ Threads/RunnableInterfaceExample.java
@@ -1,7 +1,7 @@
 package Threads;
 
 public class RunnableInterfaceExample implements Runnable{
-    //Program to implement Runnable interface example
+    //Program to implement Runnable Interface example
     @Override
     public void run(){
         System.out.println("Purchasing Laptop");
