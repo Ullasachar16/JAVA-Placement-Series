f0429af497086ed8b9cc9bb3d434ff9a879a5f2e
Ullasachar16 <ullasachar16@gmail.com>
2023-03-15 11:55:19 +0530

    Commit

--- Threads/ThreadClassExample.java
+++ Threads/ThreadClassExample.java
@@ -1,7 +1,7 @@
 package Threads;
 
 public class ThreadClassExample extends Thread {
-    //Program to show thread class example
+    //Program to show Thread class example
     @Override
     public void run() {
         for (int i = 1; i <= 5; i++) {
