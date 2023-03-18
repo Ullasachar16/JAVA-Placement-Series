abd63f108f51613165fd519416fa78894a4601a9
Ullasachar16 <ullasachar16@gmail.com>
2023-03-18 11:43:49 +0530

    Commit

--- GUI/JTableDemo.java
+++ GUI/JTableDemo.java
@@ -1,7 +1,7 @@
 package GUI;
 import javax.swing.*;
 class JTableDemo {
-    //Program to implement JTable using an example
+    //Program to Implement JTable using an example
     JFrame f;
     JTableDemo(){
         f=new JFrame();
