ec382b538b5fd2dfb46638485406109ac78c5ad0
Ullasachar16 <ullasachar16@gmail.com>
2023-03-19 18:48:18 +0530

    Commit

--- Libraries/EqualsMethodExample1.java
+++ Libraries/EqualsMethodExample1.java
@@ -1,7 +1,7 @@
 package Libraries;
 
 public class EqualsMethodExample1 {
-    //Program to show Equals Method
+    //Program to Show Equals Method
     int marks;
     public EqualsMethodExample1(int marks) {
         this.marks = marks;
