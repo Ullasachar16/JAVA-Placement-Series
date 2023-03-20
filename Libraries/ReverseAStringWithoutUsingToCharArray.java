a726c642dd3dc8b7a7b774d1a721070a9ac3d7b7
Ullasachar16 <ullasachar16@gmail.com>
2023-03-20 10:31:31 +0530

    Commit

--- Libraries/ReverseAStringWithoutUsingToCharArray.java
+++ Libraries/ReverseAStringWithoutUsingToCharArray.java
@@ -1,7 +1,7 @@
 package Libraries;
 
 public class ReverseAStringWithoutUsingToCharArray {
-    //Program to reverse string without using chararray
+    //Program to Reverse string without using chararray
     public static void main(String[] args) {
         String x="A B C D";
         for(int i=x.length()-1;i>=0;i--){
