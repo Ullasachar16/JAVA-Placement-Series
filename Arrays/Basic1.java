a0958baf277406ccedf6281bf8efaad191fb3347
Ullasachar16 <ullasachar16@gmail.com>
2023-03-21 12:17:43 +0530

    Commit

--- Arrays/Basic1.java
+++ Arrays/Basic1.java
@@ -3,7 +3,7 @@ package Arrays;
 import java.util.Scanner;
 
 public class Basic1 {
-    //Getting Array Input from the users and printing it
+    //Getting Array Input from the Users and printing it
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter array size:");
