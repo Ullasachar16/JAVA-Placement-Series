374d4e6e88f7d4a42c6211a62e6f137dfb80eabb
Ullasachar16 <ullasachar16@gmail.com>
2023-03-21 12:18:39 +0530

    Commit

--- Arrays/CreateAndDisplayMatrix.java
+++ Arrays/CreateAndDisplayMatrix.java
@@ -3,7 +3,7 @@ package Arrays;
 import java.util.Scanner;
 
 class Matrix_Create{
-    //Program to create matrix using arrays and print it
+    //Program to Create matrix using arrays and print it
     Scanner scan;
     int matrix[][];
     int row,column;
