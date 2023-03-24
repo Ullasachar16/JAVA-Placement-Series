05fe521dad71651262761c5074f56fce885ca353
Ullasachar16 <ullasachar16@gmail.com>
2023-03-24 12:56:07 +0530

    Commit

--- BasicFundamentals/BasicCalculator.java
+++ BasicFundamentals/BasicCalculator.java
@@ -65,7 +65,7 @@ public class BasicCalculator {
                 System.out.println(number1 + " - " + number2 + " = " + result);
                 break;
 
-            // Performs Multiplication between numbers
+            // Performs Multiplication Between numbers
             case '*':
                 result = number1 * number2;
                 System.out.println(number1 + " * " + number2 + " = " + result);
