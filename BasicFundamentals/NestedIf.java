1e007842ff3d2ca9dd4b7a2ed990f121683bfa34
Ullasachar16 <ullasachar16@gmail.com>
2023-03-29 12:11:34 +0530

    Commit

--- BasicFundamentals/NestedIf.java
+++ BasicFundamentals/NestedIf.java
@@ -1,7 +1,7 @@
 package BasicFundamentals;
 
 public class NestedIf {
-    //Program to Implement Nested-if
+    //Program to Implement Nested-If
     public static void main(String[] args) {
         String name="john";
         String password="john404";
