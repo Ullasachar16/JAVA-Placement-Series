118df350065b8d3098d72f2f94b9941dceff8f60
Ullasachar16 <ullasachar16@gmail.com>
2023-03-29 12:19:30 +0530

    Commit

--- BasicFundamentals/NestedIf.java
+++ BasicFundamentals/NestedIf.java
@@ -1,15 +1,9 @@
 package BasicFundamentals;
-
-import java.util.Scanner;
-
 public class NestedIf {
     //Program to Implement Nested-If
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter name:");
-        String name=sc.nextLine();
-        System.out.println("Enter password:");
-        String password=sc.nextLine();
+        String name="John";
+        String password="John404";
         if(name=="John"){
             if(password=="John404"){
                 System.out.println("Login Successful");
