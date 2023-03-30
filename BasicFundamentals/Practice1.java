e7dcf74e3e078d3b19a6222dbfc2e591e773fb2a
Ullasachar16 <ullasachar16@gmail.com>
2023-03-30 17:48:04 +0530

    Commit

--- BasicFundamentals/Practice1.java
+++ BasicFundamentals/Practice1.java
@@ -6,7 +6,7 @@ public class Practice1 {
         int n=4;
         //Upper Half
         for(int i=1;i<=n;i++) {
-            //1st part
+            //1st Part
             //stars
             for (int j = 1; j <= i; j++) {
                 System.out.print("* ");
