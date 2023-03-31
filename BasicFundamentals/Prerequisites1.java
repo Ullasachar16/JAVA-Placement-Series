2778333f03d38aaedd07f4540dde55fe115e4683
Ullasachar16 <ullasachar16@gmail.com>
2023-03-31 11:47:39 +0530

    Commit

--- BasicFundamentals/Prerequisites1.java
+++ BasicFundamentals/Prerequisites1.java
@@ -19,7 +19,7 @@ public class Prerequisites1 {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter value for a :");
         int a1=sc.nextInt();
-        System.out.println("enter value for b:");
+        System.out.println("Enter value for b:");
         int b1=sc.nextInt();
         int sum=a1+b1;
         System.out.println("sum:"+sum);
