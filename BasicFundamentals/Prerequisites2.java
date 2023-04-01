7c0d826b234bba2f6fa72363328bd3f922aba92d
Ullasachar16 <ullasachar16@gmail.com>
2023-04-01 12:38:27 +0530

    Commit

--- BasicFundamentals/Prerequisites2.java
+++ BasicFundamentals/Prerequisites2.java
@@ -14,7 +14,7 @@ public class Prerequisites2 {
         long d=10;
         char e='a';
         boolean f=true; */
-        //Program to calculate area of circle
+        //Program to Calculate area of circle
         Scanner sc=new Scanner(System.in);
         System.out.println("enter radius of the circle");
         double radius=sc.nextDouble();
