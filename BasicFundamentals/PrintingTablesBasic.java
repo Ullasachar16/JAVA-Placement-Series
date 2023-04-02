86f0b93553f9204625dd0000bc2365107e1954bb
Ullasachar16 <ullasachar16@gmail.com>
2023-04-02 12:05:52 +0530

    Commit

--- BasicFundamentals/PrintingTablesBasic.java
+++ BasicFundamentals/PrintingTablesBasic.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class PrintingTablesBasic {
     //Program to Print tables according to users input
     public static void main(String[] args) {
-        //print tables According to User's input
+        //print tables According to User's Input
         Scanner sc=new Scanner(System.in);
         System.out.println("enter which table to print:");
         int table=sc.nextInt();
