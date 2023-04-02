b860ca618226290f7963bf6068f94c520f15165b
Ullasachar16 <ullasachar16@gmail.com>
2023-04-02 12:04:51 +0530

    Commit

--- BasicFundamentals/PrintingEvenNumbersTillN.java
+++ BasicFundamentals/PrintingEvenNumbersTillN.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class PrintingEvenNumbersTillN {
     //Program to Print Even Numbers Till N using Scanner Class
     public static void main(String[] args) {
-        //Print All Even Numbers Till n
+        //Print All Even Numbers Till N
         Scanner sc=new Scanner(System.in);
         System.out.println("enter n value:");
         int n=sc.nextInt();
