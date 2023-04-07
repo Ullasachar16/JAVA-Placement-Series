0189adead32918ef594e3b2b20caaec9009ffee4
Ullasachar16 <ullasachar16@gmail.com>
2023-04-07 12:37:12 +0530

    Commit

--- BasicFundamentals/SumOfNaturalNumbersN.java
+++ BasicFundamentals/SumOfNaturalNumbersN.java
@@ -13,6 +13,6 @@ public class SumOfNaturalNumbersN {
       for(int i=1;i<=n;i++){
           sum=sum+i;
       }
-        System.out.println("sum:"+sum);
+        System.out.println("Sum:"+sum);
     }
 }
