e89cf5d93a868a597a771362fd20f5adf9ff95b7
Ullas P <112563367+Ullasachar16@users.noreply.github.com>
2023-04-08 13:37:37 +0530

    Update FontTextLabel.java

--- AWT/FontTextLabel.java
+++ AWT/FontTextLabel.java
@@ -2,7 +2,7 @@ package AWT;
 import java.awt.*;
 public class FontTextLabel extends Frame{
     //Program to Implement Font Text Label Using AWT
-    //Declare component label
+    //Declare Component label
     Label lb1,lb2;
     public FontTextLabel(){
         setLayout(new FlowLayout());
