dfadab451437e91bf9b5fbd9caa6fcee3c0e82d3
Ullas P <112563367+Ullasachar16@users.noreply.github.com>
2023-04-09 12:23:11 +0530

    Update SimpleLabel.java

--- AWT/SimpleLabel.java
+++ AWT/SimpleLabel.java
@@ -1,7 +1,7 @@
 package AWT;
 import java.awt.*;
 public class SimpleLabel extends Frame {
-    //Program to implement AWT components
+    //Program to Implement AWT components
     //Declare component label
     Label lblCount;
     public SimpleLabel(){
