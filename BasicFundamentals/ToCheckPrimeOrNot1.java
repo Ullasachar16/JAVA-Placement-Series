b7a9222fbc2aba1bf822a991ca656648e7146225
Ullasachar16 <ullasachar16@gmail.com>
2023-04-12 12:30:38 +0530

    Commit

--- BasicFundamentals/ToCheckPrimeOrNot1.java
+++ BasicFundamentals/ToCheckPrimeOrNot1.java
@@ -3,7 +3,7 @@ package BasicFundamentals;
 import java.util.*;
 
 public class ToCheckPrimeOrNot1 {
-    //Program to Check prime or not another example
+    //Program to Check Prime or not another example
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter a number");
