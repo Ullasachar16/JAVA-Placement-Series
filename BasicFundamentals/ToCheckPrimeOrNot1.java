e615d40a93879bd4980aed79b315113c80274e8d
Ullasachar16 <ullasachar16@gmail.com>
2023-04-13 14:00:48 +0530

    Commit

--- BasicFundamentals/ToCheckPrimeOrNot1.java
+++ BasicFundamentals/ToCheckPrimeOrNot1.java
@@ -6,7 +6,7 @@ public class ToCheckPrimeOrNot1 {
     //Program to Check Prime or Not Another Example
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number");
+        System.out.println("Enter a Number");
         int n = sc.nextInt();
 
         boolean isPrime = true;
