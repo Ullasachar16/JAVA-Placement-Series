e4d5db44d9a8eb65a6fbdd17ef995fd82533ffba
Ullasachar16 <ullasachar16@gmail.com>
2023-04-14 18:17:00 +0530

    Commit

--- CollectionFramework/AutoBoxingAndUnboxingDemo.java
+++ CollectionFramework/AutoBoxingAndUnboxingDemo.java
@@ -1,7 +1,7 @@
 package CollectionFramework;
 
 public class AutoBoxingAndUnboxingDemo {
-    //Program to Implement Auto Boxing and auto unboxing demo example
+    //Program to Implement Auto Boxing and Auto unboxing demo example
     public static void main(String[] args) {
         //primitive way of representing 10
         int a=10;
