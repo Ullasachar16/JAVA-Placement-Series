dea316c7450937282a2f3f9b927836b93ed42747
Ullasachar16 <ullasachar16@gmail.com>
2023-04-18 13:35:12 +0530

    Commit

--- CollectionFramework/CollectionProgramExample1.java
+++ CollectionFramework/CollectionProgramExample1.java
@@ -5,12 +5,13 @@ import java.util.LinkedList;
 public class CollectionProgramExample1 {
     //Program to Implement Collection Framework Example 2
     public static void main(String[] args) {
-        Mobile m1=new Mobile("Samsung",25000);
+        Mobile m1=new Mobile("Samsung",35000);
         LinkedList<Mobile>l=new LinkedList<Mobile>();
         l.add(m1);
         l.add(new Mobile("Xiomi",18000));
         l.add(new Mobile("Realme",15000));
         l.add(new Mobile("iPhone",125000));
+        l.add(new Mobile("Vivo",28000));
         for(Mobile m:l){
             if(m.cost>=10000&&m.cost<=25000){
                 System.out.println(m);
