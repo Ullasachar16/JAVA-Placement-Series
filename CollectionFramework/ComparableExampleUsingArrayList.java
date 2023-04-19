3e3148e64facc837b2c11274b4ea9e364908fce0
Ullasachar16 <ullasachar16@gmail.com>
2023-04-19 11:15:48 +0530

    Commit

--- CollectionFramework/ComparableExampleUsingArrayList.java
+++ CollectionFramework/ComparableExampleUsingArrayList.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 class Mobile1 implements Comparable<Mobile1>{
-    //Program to Show Comparable interface working
+    //Program to Show Comparable Interface working
     int cost;
     Mobile1(int cost){
         this.cost=cost;
