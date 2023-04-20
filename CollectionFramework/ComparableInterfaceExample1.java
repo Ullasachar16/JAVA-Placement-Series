d1d97e5c953f0ac31a65a2442d8c77531d93e49e
Ullasachar16 <ullasachar16@gmail.com>
2023-04-20 11:10:34 +0530

    Commit

--- CollectionFramework/ComparableInterfaceExample1.java
+++ CollectionFramework/ComparableInterfaceExample1.java
@@ -21,7 +21,7 @@ class Employee implements Comparable<Employee>{
 }
 
 public class ComparableInterfaceExample1 {
-    //Another Example Program to show comparable interface working
+    //Another Example Program to Show comparable interface working
     public static void main(String[] args) {
         Employee e1=new Employee("Tom",45.5);
         TreeSet<Employee>t=new TreeSet<>();
