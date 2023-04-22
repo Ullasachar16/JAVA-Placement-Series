5192ded5a0233bef2ef6bdc060264c28078ae86e
Ullasachar16 <ullasachar16@gmail.com>
2023-04-22 13:16:11 +0530

    Commit

--- CollectionFramework/DemoArrayList.java
+++ CollectionFramework/DemoArrayList.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class DemoArrayList {
     public static void main(String[] args) {
         //Demo Program of ArrayList
-        //Pre-Defined Methods of List Interface and Arraylist class
+        //Pre-Defined Methods of List Interface and ArrayList class
         ArrayList l=new ArrayList();
         l.add(10);
         l.add(20);
