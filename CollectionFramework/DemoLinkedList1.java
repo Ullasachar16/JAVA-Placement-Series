86c593fad7d6d66682d9a6c8b3ac09d5225d7600
Ullasachar16 <ullasachar16@gmail.com>
2023-04-23 11:12:28 +0530

    Commit

--- CollectionFramework/DemoLinkedList1.java
+++ CollectionFramework/DemoLinkedList1.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class DemoLinkedList1 {
-    //Another Demo program for linked-list
+    //Another Demo Program for linked-list
     public static void main(String[] args) {
         ArrayList al=new ArrayList();
         al.add(10);
