4212253db3ddb6164b92c5aaaa0311e15920708a
Ullasachar16 <ullasachar16@gmail.com>
2023-04-26 10:37:35 +0530

    Commit

--- CollectionFramework/MapInterfaceAllClassesExample.java
+++ CollectionFramework/MapInterfaceAllClassesExample.java
@@ -6,7 +6,7 @@ import java.util.Set;
 import java.util.TreeMap;
 
 public class MapInterfaceAllClassesExample {
-    //Program to Show the Execution and implementation of map-interface
+    //Program to Show the Execution and Implementation of map-interface
     public static void main(String[] args) {
         HashMap<String,Integer> h=new HashMap();
         h.put("Tom",22);
