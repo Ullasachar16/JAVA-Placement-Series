491910b0483ec0c20ac34d01469758d469d765b7
Ullasachar16 <ullasachar16@gmail.com>
2023-04-29 11:46:29 +0530

    Commit

--- CollectionFramework/MapInterfaceUsingHashMapAndMethodsExample.java
+++ CollectionFramework/MapInterfaceUsingHashMapAndMethodsExample.java
@@ -3,7 +3,7 @@ package CollectionFramework;
 import java.util.HashMap;
 
 public class MapInterfaceUsingHashMapAndMethodsExample {
-    //Map Interface example program using hashmap
+    //Map Interface Example program using hashmap
     public static void main(String[] args) {
         HashMap h=new HashMap();
         //put() is used to add key and value inside map
