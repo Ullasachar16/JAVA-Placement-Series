234cc5dfbbe96c26e925d144db7e554e2421bd01
Ullasachar16 <ullasachar16@gmail.com>
2023-04-30 12:37:32 +0530

    Commit

--- CollectionFramework/TreeSetExample.java
+++ CollectionFramework/TreeSetExample.java
@@ -3,7 +3,7 @@ package CollectionFramework;
 import java.util.TreeSet;
 
 public class TreeSetExample {
-    //Tree Set Example Program using collections
+    //Tree Set Example Program Using collections
     public static void main(String[] args) {
         TreeSet t=new TreeSet();
         t.add(40);
