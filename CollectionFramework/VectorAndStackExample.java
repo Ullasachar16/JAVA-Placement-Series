99b43ca6f3403d9df950a6ba2f87ae9c636a20d1
Ullasachar16 <ullasachar16@gmail.com>
2023-04-30 12:39:10 +0530

    Commit

--- CollectionFramework/VectorAndStackExample.java
+++ CollectionFramework/VectorAndStackExample.java
@@ -6,7 +6,7 @@ import java.util.Stack;
 import java.util.Vector;
 
 public class VectorAndStackExample {
-    //Vector and Stack Example Program Using Collections
+    //Vector and Stack Example Program Using Collection Framework
     public static void main(String[] args) {
         ArrayList al=new ArrayList();
         al.add(10);
