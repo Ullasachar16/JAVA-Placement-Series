4f1e8148c6376a5d3409aa7c35bdeee86a407c9c
Ullasachar16 <ullasachar16@gmail.com>
2023-05-06 11:25:29 +0530

    Commit

--- CustomException/InsufficientBalanceExceptionInCheckException.java
+++ CustomException/InsufficientBalanceExceptionInCheckException.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class InsufficientBalanceExceptionInCheckException extends Exception {
 }
 class ATM{
-    //Program to Implement Insufficient-Balance-Exception using Check exception
+    //Program to Implement Insufficient-Balance-Exception using Check Exception
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         double balance=10000;
