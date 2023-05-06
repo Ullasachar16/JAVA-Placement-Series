762cf0b2e17e2efd8f701a3de1455fc79b0f2d86
Ullasachar16 <ullasachar16@gmail.com>
2023-05-06 11:25:39 +0530

    Commit

--- CustomException/InsufficientBalanceExceptionInCheckException.java
+++ CustomException/InsufficientBalanceExceptionInCheckException.java
@@ -9,7 +9,7 @@ class ATM{
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         double balance=10000;
-        System.out.println("Enter amt to be withdrawn:");
+        System.out.println("Enter Amount to be withdrawn:");
         double amount=sc.nextDouble();
         if(amount<=balance){
             System.out.println("Withdrawn successfully");
