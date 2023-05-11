ecbfb107537faef5193d211ebd9ea5640686b984
Ullasachar16 <ullasachar16@gmail.com>
2023-05-11 11:23:37 +0530

    Commit

--- DataStructures/ArrayLists.java
+++ DataStructures/ArrayLists.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class ArrayLists {
-    //Arraylist example
+    //ArrayList example
     public static void main(String args[]) {
         ArrayList<Integer> list = new ArrayList<Integer>();
 //        ArrayList<String> list2 = new ArrayList<String>();
