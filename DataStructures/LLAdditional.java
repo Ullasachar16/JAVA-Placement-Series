d0302ab88c6b4852648f1cda20e920550037bff2
Ullasachar16 <ullasachar16@gmail.com>
2023-05-17 13:00:05 +0530

    Commit

--- DataStructures/LLAdditional.java
+++ DataStructures/LLAdditional.java
@@ -55,7 +55,7 @@ class LLAdditional {
     }
     public void removeLast() {
         if(head == null) {
-            System.out.println("Empty List, nothing to delete");
+            System.out.println("Empty List, Nothing to Delete");
             return;
         }
         size--;
