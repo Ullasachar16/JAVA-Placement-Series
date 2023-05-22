87914b86fe7ff4c4f9e2cf8dde4a23e52ac41e72
Ullasachar16 <ullasachar16@gmail.com>
2023-05-22 12:29:45 +0530

    Commit

--- DataStructures/StackProblem2.java
+++ DataStructures/StackProblem2.java
@@ -21,7 +21,7 @@ public class StackProblem2 {
         pushAtBottom(s, top);
     }
     public static void main(String args[]) {
-        //using stack data structure
+        //Using stack data structure
         Stack<Integer> stack = new Stack<>();
         stack.push(1);
         stack.push(2);
