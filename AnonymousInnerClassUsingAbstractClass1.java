0fb5df60a52a76d72c8c0635f1abadf8eea3a0cb
Ullasachar16 <ullasachar16@gmail.com>
2023-05-24 12:03:26 +0530

    Commit

--- AnonymousInnerClassUsingAbstractClass1.java
+++ AnonymousInnerClassUsingAbstractClass1.java
@@ -1,5 +1,5 @@
 public class AnonymousInnerClassUsingAbstractClass1 {
-    //Program to implement anonymous inner class example 2
+    //Program to Implement anonymous inner class example 2
     public static void main(String[] args) {
         Car c=new Car(){
             @Override
