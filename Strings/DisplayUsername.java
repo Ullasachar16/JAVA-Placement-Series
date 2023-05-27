40ac9ad3977e2ced36dad9ad41167f14ef8af84e
Ullasachar16 <ullasachar16@gmail.com>
2023-05-27 12:17:50 +0530

    Commit

--- Strings/DisplayUsername.java
+++ Strings/DisplayUsername.java
@@ -3,7 +3,7 @@ package Strings;
 import java.util.Scanner;
 
 public class DisplayUsername {
-    //Program to Print Username inputted by the user
+    //Program to Print Username Inputted by the user
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter String:");
