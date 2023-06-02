c61bf89e9775aae210ef5aa10114ae66ace63641
Ullasachar16 <ullasachar16@gmail.com>
2023-06-02 11:47:06 +0530

    Commit

--- Threads/GarbageCollectionExample.java
+++ Threads/GarbageCollectionExample.java
@@ -3,7 +3,7 @@ package Threads;
 public class GarbageCollectionExample {
     //Example Program to Show Garbage Collection Implementation
     protected void finalize(){
-        System.out.println("finalize method executing before garbage collecting");
+        System.out.println("Finalize method executing before garbage collecting");
     }
     public static void main(String[] args) {
         GarbageCollectionExample g=new GarbageCollectionExample();
