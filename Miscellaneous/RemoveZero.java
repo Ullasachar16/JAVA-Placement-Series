2ef0ecbbfe6e9adbb5346fb23c8cf483df74ba1a
Ullasachar16 <ullasachar16@gmail.com>
2023-06-14 12:43:15 +0530

    Commit

--- Miscellaneous/RemoveZero.java
+++ Miscellaneous/RemoveZero.java
@@ -3,7 +3,7 @@ package Miscellaneous;
 import java.util.Scanner;
 
 public class RemoveZero {
-    //Program to Remove Zeroes that are Provided in the input
+    //Program to Remove Zeroes that are Provided in the Input
     public static void main(String[] args) {
         int a,x,b;
         char z;
