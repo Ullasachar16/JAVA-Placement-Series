8efecd9eb9e96bf2ffdcbffbe971687695c9dbf8
Ullasachar16 <ullasachar16@gmail.com>
2023-06-26 13:01:23 +0530

    Commit

--- Arrays/MaxAndMinNumberInArray.java
+++ Arrays/MaxAndMinNumberInArray.java
@@ -3,7 +3,7 @@ package Arrays;
 import java.util.Scanner;
 
 public class MaxAndMinNumberInArray {
-    //Program to find Max and Min Number in array
+    //Program to find Max and Min Number in Array
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter array size:");
