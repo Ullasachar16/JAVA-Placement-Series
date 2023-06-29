7cc9009b59cc62de12f09b6f32f13b61b753361f
Ullas P <ullasachar16@gmail.com>
2023-06-29 13:05:55 +0530

    Commit

--- Arrays/PrintArrayNames.java
+++ Arrays/PrintArrayNames.java
@@ -6,7 +6,7 @@ public class PrintArrayNames {
     //Program to print array names inputted by the user
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter Array size:");
+        System.out.println("Enter Array Size:");
         int size= sc.nextInt();
         String names[]=new String[size];
         System.out.println("Enter names:");
