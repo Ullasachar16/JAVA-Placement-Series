9446a5919afbb7810d358e59f5ba1df8b4882b73
Ullas P <112563367+Ullasachar16@users.noreply.github.com>
2023-07-03 12:35:56 +0530

    Commit

--- ExceptionHandling/DemoArithmeticException.java
+++ ExceptionHandling/DemoArithmeticException.java
@@ -3,7 +3,7 @@ package ExceptionHandling;
 import java.util.Scanner;
 
 public class DemoArithmeticException {
-    //Implementing Arithmetic exception
+    //Implementing Arithmetic Exception
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter a:");
