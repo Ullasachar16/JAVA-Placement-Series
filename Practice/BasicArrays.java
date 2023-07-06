ae57a2e3d1297412b669aaf2856f58552bbedf2d
Ullas P <ullasachar16@gmail.com>
2023-07-06 14:32:43 +0530

    Commit

--- Practice/BasicArrays.java
+++ Practice/BasicArrays.java
@@ -15,7 +15,7 @@ public class BasicArrays {
     }
     public static int[] getIntegers(int number){
         //To get integers from array
-        System.out.println("Enter "+number+ " Integer value");
+        System.out.println("Enter "+number+ " Integer Value");
         int[] values=new int[number];
         for(int i=0;i<values.length;i++){
             values[i]=sc.nextInt();
