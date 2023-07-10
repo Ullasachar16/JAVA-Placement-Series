812def2dac6cf8d88ddedb3e852a0986a5d4c0ea
Ullas P <ullasachar16@gmail.com>
2023-07-10 12:36:19 +0530

    Commit

--- Practice/BasicArrays.java
+++ Practice/BasicArrays.java
@@ -6,7 +6,7 @@ public class BasicArrays {
     //Program to Implement Basic Arrays
     private static Scanner sc=new Scanner(System.in);
     public static void main(String[] args) {
-        //Main function
+        //Main Function
         int[] arr=getIntegers(5);
         for(int i=0;i<arr.length;i++){
             System.out.println("Element "+i+" , Typed Value was "+arr[i]);
