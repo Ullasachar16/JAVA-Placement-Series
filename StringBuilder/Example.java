7b7e231c87783f46a06583082832f077bffa593b
Ullasachar16 <ullasachar16@gmail.com>
2023-08-03 14:01:20 +0530

    Commit

--- StringBuilder/Example.java
+++ StringBuilder/Example.java
@@ -1,7 +1,7 @@
 package StringBuilder;
 
 public class Example {
-    //Example Program of string builder to allocate delete reverse and other factors
+    //Example Program of String builder to allocate delete reverse and other factors
     public static void main(String[] args) {
         StringBuilder sb=new StringBuilder("Java");
         //how original string is changed
