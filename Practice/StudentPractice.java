9a52141f6b0fb2e51bb2ea1f0b659c43a927ae9b
Ullasachar16 <ullasachar16@gmail.com>
2023-08-05 14:38:52 +0530

    Commit

--- Practice/StudentPractice.java
+++ Practice/StudentPractice.java
@@ -18,7 +18,7 @@ public class StudentPractice {
     }
 }
 class Soluto{
-    //Main class for Constructor Overloading
+    //Main Class for Constructor Overloading
     public static void main(String[] args) {
         StudentPractice s1=new StudentPractice();
         s1.setAge(35);
