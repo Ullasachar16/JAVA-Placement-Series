69224cfa67ef656a8de8789c9816c3fdb5f8323c
Ullasachar16 <ullasachar16@gmail.com>
2023-08-11 12:55:52 +0530

    Commit

--- OperatorsAndBitManipulation/BitwiseOperator.java
+++ OperatorsAndBitManipulation/BitwiseOperator.java
@@ -7,7 +7,7 @@ public class BitwiseOperator {
         int a=5,b=4;
         System.out.println(a&b);
 
-        //binary OR
+        //Binary OR
         System.out.println(a|b);
 
         //binary XOR
