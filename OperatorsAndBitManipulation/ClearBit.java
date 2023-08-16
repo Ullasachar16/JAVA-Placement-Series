c71f81f817ab48ef86ef6965fbb9c6c387ff11c8
Ullasachar16 <ullasachar16@gmail.com>
2023-08-16 12:18:42 +0530

    Commit

--- OperatorsAndBitManipulation/ClearBit.java
+++ OperatorsAndBitManipulation/ClearBit.java
@@ -1,7 +1,7 @@
 package OperatorsAndBitManipulation;
 
 public class ClearBit {
-    //Program to Show Implementation clear bit
+    //Program to Show Implementation of clear bit
     public static void main(String[] args) {
         //Clear Bit
         int n = 5;
