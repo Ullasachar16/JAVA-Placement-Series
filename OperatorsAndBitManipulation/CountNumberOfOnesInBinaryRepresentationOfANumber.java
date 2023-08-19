4b2f2560665bdc881bc4a29f0872acaed11c9307
Ullasachar16 <ullasachar16@gmail.com>
2023-08-19 12:46:17 +0530

    Commit

--- OperatorsAndBitManipulation/CountNumberOfOnesInBinaryRepresentationOfANumber.java
+++ OperatorsAndBitManipulation/CountNumberOfOnesInBinaryRepresentationOfANumber.java
@@ -19,7 +19,7 @@ public class CountNumberOfOnesInBinaryRepresentationOfANumber {
         // Counter for Binary Array
         int i = 0;
         while (n > 0) {
-            // Storing remainder in binary array
+            // Storing Remainder in binary array
             binaryNum[i] = n % 2;
             n = n / 2;
             i++;
