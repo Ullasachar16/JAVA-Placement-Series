edfa065d5929987eff1fce38c7ff9d2180989481
Ullas P <ullasachar16@gmail.com>
2023-08-22 16:11:32 +0530

    Commit

--- OperatorsAndBitManipulation/DecimalToBinaryAndBinaryToDecimal.java
+++ OperatorsAndBitManipulation/DecimalToBinaryAndBinaryToDecimal.java
@@ -9,7 +9,7 @@ public class DecimalToBinaryAndBinaryToDecimal {
         // Counter for Binary Array
         int i = 0;
         while (n > 0) {
-            // Storing remainder in binary array
+            // Storing Remainder in binary array
             binaryNum[i] = n % 2;
             n = n / 2;
             i++;
