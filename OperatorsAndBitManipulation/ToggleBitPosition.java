769c2366de27ccd25ff2290368a109433778257c
Ullasachar16 <ullasachar16@gmail.com>
2023-09-02 18:39:59 +0530

    Commit

--- OperatorsAndBitManipulation/ToggleBitPosition.java
+++ OperatorsAndBitManipulation/ToggleBitPosition.java
@@ -6,7 +6,7 @@ public class ToggleBitPosition {
     //Program to Toggle Bit Position
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter bit position:");
+        System.out.println("Enter bit position:");
         int n=5;
         int pos = sc.nextInt();
         int bitmask= 1<<pos;
