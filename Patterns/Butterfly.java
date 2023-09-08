46966595b0bacb66754e1075ecacf3bd64fc8e2f
Ullasachar16 <ullasachar16@gmail.com>
2023-09-08 18:42:00 +0530

    Commit

--- Patterns/Butterfly.java
+++ Patterns/Butterfly.java
@@ -3,7 +3,7 @@ package Patterns;
 public class Butterfly {
     public static void main(String[] args) {
         //Program to Print Butterfly Pattern
-        //Print Butterfly pattern using stars
+        //Print Butterfly Pattern using stars
         int n=4;
         //upper part
         for(int i=1;i<=n;i++){
