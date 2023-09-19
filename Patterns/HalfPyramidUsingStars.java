c2d0fbe48853683a636ecc588d5682d02aaa58e6
Ullasachar16 <ullasachar16@gmail.com>
2023-09-19 11:44:02 +0530

    Commit

--- Patterns/HalfPyramidUsingStars.java
+++ Patterns/HalfPyramidUsingStars.java
@@ -3,7 +3,7 @@ package Patterns;
 public class HalfPyramidUsingStars {
     //Program to Print Half Pyramid Pattern using Stars
     public static void main(String[] args) {
-        //Print Half Pyramid pattern
+        //Print Half Pyramid Pattern
         int n=4;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
