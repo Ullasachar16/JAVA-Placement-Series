b9fa0cd41a2ae70766c7c21bcc9e5da54afcc93f
Ullasachar16 <ullasachar16@gmail.com>
2023-09-26 19:35:45 +0530

    Commit

--- Patterns/InvertedHalfPyramidUsingNumbers.java
+++ Patterns/InvertedHalfPyramidUsingNumbers.java
@@ -16,7 +16,7 @@ public class InvertedHalfPyramidUsingNumbers {
             }
             System.out.println();
         }
-        //Another solution
+        //Another Solution
         for(int i=1;i<=n;i++){
             for(int j=1;j<=(n-i)+1;j++){
                 System.out.print(j+" ");
