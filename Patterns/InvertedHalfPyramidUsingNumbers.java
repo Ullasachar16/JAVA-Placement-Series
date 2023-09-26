96570c61eec84a8eafb03ba61e61eb57107e8149
Ullasachar16 <ullasachar16@gmail.com>
2023-09-26 19:35:28 +0530

    Commit

--- Patterns/InvertedHalfPyramidUsingNumbers.java
+++ Patterns/InvertedHalfPyramidUsingNumbers.java
@@ -1,7 +1,7 @@
 package Patterns;
 
 public class InvertedHalfPyramidUsingNumbers {
-    //Another program for Printing Inverted Half Pyramid using numbers
+    //Another program for Printing Inverted Half Pyramid using Numbers
     public static void main(String[] args) {
          /* Print Inverted Half Pyramid pattern using numbers
                     1 2 3 4
