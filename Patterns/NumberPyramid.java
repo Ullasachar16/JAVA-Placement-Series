10f4b84821d00ca4ae9d5a10eaa5092b6ace47c3
Ullasachar16 <ullasachar16@gmail.com>
2023-10-03 11:08:06 +0530

    Commit

--- Patterns/NumberPyramid.java
+++ Patterns/NumberPyramid.java
@@ -3,7 +3,7 @@ package Patterns;
 public class NumberPyramid {
     //Program to Print Number Pyramid Pattern
     public static void main(String[] args) {
-        //Print Number Pyramid Triangle pattern
+        //Print Number Pyramid Triangle Pattern
         int n=5;
         for(int i=1;i<=n;i++){
             //Spaces
