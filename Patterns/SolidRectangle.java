dcabde46ee1c0c945b5166ecbab95bbbdc040808
Ullas <ullasachar16@gmail.com>
2023-10-24 16:15:24 +0530

    Commit

--- Patterns/SolidRectangle.java
+++ Patterns/SolidRectangle.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class SolidRectangle {
     //Program to Print Solid Rectangle Pattern
     public static void main(String[] args) {
-        //Printing Solid Rectangle pattern
+        //Printing Solid Rectangle Pattern
         Scanner sc=new Scanner(System.in);
         System.out.println("enter no of rows");
         int n=sc.nextInt();
