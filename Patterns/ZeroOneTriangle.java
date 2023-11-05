d6788f198a515f039bd53811c7d71252cebe2314
Ullas <ullasachar16@gmail.com>
2023-11-05 12:19:13 +0530

    Commit

--- Patterns/ZeroOneTriangle.java
+++ Patterns/ZeroOneTriangle.java
@@ -3,7 +3,7 @@ package Patterns;
 public class ZeroOneTriangle {
     //Program to Print Zero-One Triangle Pattern
     public static void main(String[] args) {
-        //Print 0-1 Triangle
+        //Print "0-1 Triangle
         int n=5;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
