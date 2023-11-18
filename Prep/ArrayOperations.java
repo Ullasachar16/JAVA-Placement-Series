0ca74a20f87202d784f1598e86321eaaedb316cc
Ullas <ullasachar16@gmail.com>
2023-11-18 11:38:01 +0530

    Commit

--- Prep/ArrayOperations.java
+++ Prep/ArrayOperations.java
@@ -5,7 +5,7 @@ public class ArrayOperations {
     public static void main(String[] args) {
         double[] myList={1.9,2.9,3.4,3.5};
         double total=0;
-        System.out.println("Array elements are:");
+        System.out.println("Array Elements are:");
         for(int i=0;i<myList.length;i++){
             System.out.println(myList[i]+" ");
         }
