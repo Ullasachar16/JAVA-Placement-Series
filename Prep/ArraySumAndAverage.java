952c77af6e9568703f037d0a5d50b6cf62eec667
Ullas <ullasachar16@gmail.com>
2023-12-02 10:52:52 +0530

    Commit

--- Prep/ArraySumAndAverage.java
+++ Prep/ArraySumAndAverage.java
@@ -1,7 +1,7 @@
 package Prep;
 
 public class ArraySumAndAverage {
-    //Program to find the Sum and Average of elements inside array
+    //Program to Find the Sum and Average of elements inside array
     public static void main(String[] args) {
         int a[]={1,4,7,9,13,15,17};
         int sum=0;
