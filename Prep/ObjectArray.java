ac6e5b3f495ad61c6d9ec416d823d011802562fc
Ullas <ullasachar16@gmail.com>
2023-12-31 18:41:06 +0530

    Commit

--- Prep/ObjectArray.java
+++ Prep/ObjectArray.java
@@ -13,7 +13,7 @@ class Employee2 {
         try{
             System.out.print("Enter Name:\n");
             name=br.readLine();
-            System.out.println("Enter Emp id:");
+            System.out.println("Enter Emp Id:");
             emp_id= Integer.parseInt(br.readLine());
             System.out.println("Enter salary:");
             salary=Integer.parseInt(br.readLine());
